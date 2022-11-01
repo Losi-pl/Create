cfc6b4cbec59e5d9cb2fcd2795c43ae97aca2ccd
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2022-11-01 19:24:18 +0100

    `Chunk` memory performance

--- Create/Space/Chunk.cs
+++ Create/Space/Chunk.cs
@@ -8,7 +8,7 @@ public sealed class Chunk
 {
     public const int QUARD_SIZE = 16, QUARD_STACK = 16, CHUNK_HEIGHT = QUARD_SIZE * QUARD_STACK;
 
-    PlacedBlock[,,] blocks = new PlacedBlock[QUARD_SIZE, QUARD_SIZE * QUARD_STACK, QUARD_SIZE];
+    PlacedBlock[][,,] blocks = new PlacedBlock[QUARD_STACK][,,];
     uint[] block_content = new uint[QUARD_STACK];
     List<int> modified_quard = new();
     object task_lock = new();
@@ -16,21 +16,49 @@ public sealed class Chunk
 
     public PlacedBlock this[int x, int y, int z]
     {
-        get => blocks[x, y, z]; 
+        get
+        {
+            lock (task_lock)
+            {
+                var y_ = y / QUARD_SIZE;
+                var qu = blocks[y_];
+                if (qu != null)
+                    return qu[x, y % QUARD_SIZE, z];
+                else
+                    return new(Blocks.AIR);
+            }
+        }
         set
         {
-            int d_index = y / QUARD_SIZE;
-            lock(task_lock) 
+            lock (task_lock)
             {
-                var old = blocks[x, y, z];
-                blocks[x, y, z] = value;
-                int q_m = y / QUARD_SIZE;
-                if(!modified_quard.Contains(q_m))
-                    modified_quard.Add(q_m);
-                if (old.Block == Blocks.AIR && value.Block != Blocks.AIR)
-                    block_content[d_index]++;
-                else if (old.Block != Blocks.AIR && value.Block == Blocks.AIR)
-                    block_content[d_index]--;
+                var y_ = y / QUARD_SIZE;
+                var Y = y % QUARD_SIZE;
+                var qu = blocks[y_];
+                if (value.Block == Blocks.AIR)
+                {
+                    if (qu == null)
+                        return;
+                    var old = qu[x, Y, z];
+                    qu[x,Y,z] = value;
+                    if (old.Block != Blocks.AIR)
+                        block_content[y_]--;
+                    if (block_content[y_] == 0)
+                        blocks[y_] = null!;
+                }
+                else
+                {
+                    if(qu == null)
+                    {
+                        PlacedBlock[,,] bls = new PlacedBlock[QUARD_SIZE, QUARD_SIZE, QUARD_SIZE];
+                        blocks[y_] = bls;
+                        qu = bls;
+                    }
+                    var old = qu[x, Y, z];
+                    qu[x, Y, z] = value;
+                    if (old.Block == Blocks.AIR)
+                        block_content[y_]++;
+                }
             }
         }
     }
