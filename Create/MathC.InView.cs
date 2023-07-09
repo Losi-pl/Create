6ff7b540baea9d33b7b5b98186e387d1980282b6
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2023-07-09 23:24:32 +0200

    Method `MathC.InView`

---
+++ Create/MathC.InView.cs
@@ -0,0 +1,11 @@
+﻿using OpenTK.Mathematics;
+
+namespace Create;
+
+partial class MathC
+{
+    public static bool InView(Matrix4 projection, ((float x, float y, float z) pozition, (float x, float y, float z) size) cube)
+    {
+        return true;
+    }
+}
