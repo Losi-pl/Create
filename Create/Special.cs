3a9b97c09e88f5b474ef54353290c7d15cfce391
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2023-07-21 21:43:52 +0200

    Special method `Contains` for `Range`

--- Create/Special.cs
+++ Create/Special.cs
@@ -105,4 +105,17 @@ internal static partial class Special
         var poi = (MathF.Abs(v1.X - v2.X), MathF.Abs(v1.Z - v2.Z));
         return MathF.Sqrt((poi.Item1 * poi.Item1) + (poi.Item2 * poi.Item2));
     }
+
+    /// <summary>
+    /// Sprawdza czy wartość <paramref name="value"/> jest w przedziale
+    /// </summary>
+    /// <param name="range"></param>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public static bool Contains(this Range range, int value)
+    {
+        if(range.Start.Value > value) return false;
+        if (range.End.Value < value) return false;
+        return true;
+    }
 }
