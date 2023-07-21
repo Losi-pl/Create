303a3fd5215573bf3b1a6536be41ecb9c143f448
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2023-07-22 00:01:09 +0200

    `Phizic` method in `Interface`

--- Create.OpenGL/GUI/Interface.cs
+++ Create.OpenGL/GUI/Interface.cs
@@ -13,6 +13,7 @@ public sealed class Interface
     public event Func<(int x, int y)>? CursorGet;
     public event Func<(bool up, bool down, bool status)>? MouseLeft;
     public event Func<(bool up, bool down, bool status)>? MouseRight;
+    SpacePoint last_hovered_over;
 
     public Interface(int width, int height)
     {
@@ -77,5 +78,63 @@ public sealed class Interface
         var cursor = (CursorGet ?? (() => (0, 0))).Invoke();
         var mouseleft = (MouseLeft ?? (() => (false, false, false))).Invoke();
         var mouseright = (MouseRight ?? (() => (false, false, false))).Invoke();
+
+        var element = pointingAt(main, new())!;
+
+        if (mouseleft.down || mouseright.down)
+            if (element is not null)
+            {
+                var el = element;
+                while (el.Parent is not null)
+                {
+                    el._onClick?.Invoke(el);
+                    el = el.Parent;
+                }
+            }
+
+        var hoverable = element;
+
+        while (hoverable is null ? false : hoverable._onEnter is null && hoverable._onExit is null)
+            hoverable = hoverable!.Parent;
+
+        if (hoverable is not null)
+        {
+            if (last_hovered_over != hoverable)
+                last_hovered_over?._onExit?.Invoke(last_hovered_over);
+
+            hoverable._onEnter?.Invoke(hoverable);
+            last_hovered_over = hoverable;
+        }
+        else if (last_hovered_over is not null)
+        {
+            last_hovered_over?._onExit?.Invoke(last_hovered_over);
+            last_hovered_over = null!;
+        }
+
+        SpacePoint? pointingAt(SpacePoint space, Vector2 parent)
+        {
+            if (!space.Active)
+                return null;
+
+            var l_poz = space.Parent is not null ? 
+                (space.Parent!.Size.ToVector() / -2) + 
+                    (space.Parent!.Size.ToVector() * ((space.AnkerPoints.point1 + space.AnkerPoints.point2) / 2)) + 
+                    space.Pozition.ToVector() :
+                (main.Size.ToVector() / -2) + (main.Size.ToVector() * ((space.AnkerPoints.point1 + space.AnkerPoints.point2) / 2)) + space.Pozition.ToVector();
+            l_poz = parent + l_poz;
+
+            foreach(var sp in space.Childs.GetEnumerable().Reverse())
+            {
+                var v = pointingAt(sp, l_poz);
+                if (v is not null)
+                    return v;
+            }
+            
+            var dis = (MathF.Abs(l_poz.X - cursor.x), MathF.Abs(l_poz.Y - cursor.y));
+            if (dis.Item1 < space.Size.Width / 2 && dis.Item2 < space.Size.Height / 2)
+                if (space != main)
+                    return space;
+            return null;
+        }
     }
 }
