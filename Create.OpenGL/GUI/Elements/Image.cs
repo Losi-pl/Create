3bfb758f230b837625e78376505d98ca551e1c52
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2023-07-23 23:37:15 +0200

    Move `Image` shader to resources

--- Create.OpenGL/GUI/Elements/Image.cs
+++ Create.OpenGL/GUI/Elements/Image.cs
@@ -5,43 +5,25 @@ namespace Create.OpenGL.GUI.Elements;
 
 public class Image : Element
 {
-    static Shader shader = Shader.Create()
-        .VertexCode(@"#version 440 core
-            in vec2 poz;
+    static Shader shader = null!;
+    static Mesh mesh = null!;
 
-            uniform mat4 matrix;
+    Color4 color = Color4.White;
+    Texture2D? texture;
 
-            out vec2 uv;
+    public Texture2D? Texture { get => texture; set => texture = value; }
+    public Color4 Color { get => color; set => color = value; }
 
-            void main()
-            {
-                uv = poz;
-                gl_Position = matrix * vec4(poz - vec2(.5, .5), 0.0, 1.0);
-            }")
-        .FragmentCode(@"#version 440 core
-            in vec2 uv;
-
-            uniform vec4 color;
-            uniform sampler2D tex;
-            uniform bool use_tex;
-
-            out vec4 color_o;
-
-            void main()
-            {
-                if (use_tex)
-                    color_o = texture(tex, uv) * color;
-                else
-                    color_o = color;
-            }")
-        .ProjectionMatrixUniform("matrix")
-        .AlphaTest()
-        .Blend(BlendingFactor.One, BlendingFactor.OneMinusSrcAlpha)
-        .DepthTest(false)
-        .Finish()
-        .SetUniform("color", new Vector4(255, 255, 255, 255));
-
-    static Mesh mesh = Mesh.Create(shader)
+    internal static void set_shader(Shader shader)
+    {
+        if(Image.shader is not null)
+        {
+            Image.mesh.Dispose();
+            Image.shader.Dispose();
+        }
+        
+        Image.shader = shader.SetUniform("color", (Vector4)Color4.White);
+        Image.mesh = Mesh.Create(shader)
         .SetVertex("poz", new Vector2[]
         {
             new (0, 0),
@@ -55,15 +37,12 @@ public class Image : Element
             0,3,1
         })
         .Finish();
-
-    Color4 color = Color4.White;
-    Texture2D? texture;
-
-    public Texture2D? Texture { get => texture; set => texture = value; }
-    public Color4 Color { get => color; set => color = value; }
+    }
 
     public override void Draw(Matrix4 projection)
     {
+        if (shader is null)
+            return;
         if(texture != null)
             shader.SetUniform("tex", texture);
         shader.SetUniform("use_tex", texture != null);
