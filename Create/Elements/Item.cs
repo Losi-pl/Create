1c110834026ab60fb7cf2a23c821408bb52584ab
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2023-07-26 23:49:07 +0200

    Method `GetItemModel` in `Item`

--- Create/Elements/Item.cs
+++ Create/Elements/Item.cs
@@ -1,3 +1,7 @@
+using Create.Conteiner;
+using Create.OpenGL;
+using OpenTK.Mathematics;
+
 namespace Create.Elements;
 
 /// <summary>
@@ -8,4 +12,11 @@ public abstract class Item : Baze
     //Ustawienie bazowego typu elementu na Item
     public sealed override Type ElementBazicType => typeof(Item);
 
+    public virtual ItemModel GetItemModel(ItemStack itemStack, Net.Player player) => new();
+
+    public struct ItemModel
+    {
+        public IDrawable model;
+        public (Color4 color, float progress)? statusBar;
+    }
 }
