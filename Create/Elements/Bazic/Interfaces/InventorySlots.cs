1f51b00848ac33703d4b8cba76738b74e98c91dd
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2023-08-02 22:44:25 +0200

    Better item transfering

--- Create/Elements/Bazic/Interfaces/InventorySlots.cs
+++ Create/Elements/Bazic/Interfaces/InventorySlots.cs
@@ -43,22 +43,96 @@ public sealed class InventorySlots
         var slot = point.Element as ItemSlot;
         if (slot is null)
             return;
+        var inv = playerInventory;
+        var tools = toolBar;
+        var transfered = transferredItem;
+        switch (args)
+        {
+            case ClickEventButton.Left:
+                var sItem = get_item();
+                if (sItem.HasValue && transfered.HasValue && 
+                   (sItem?.Item == transfered?.Item) &&
+                   (sItem?.Type == transfered?.Type) &&
+                   (sItem?.Meta == transfered?.Meta))
+                {
+                    var sum = sItem!.Value.Count + transfered!.Value.Count;
+                    uint ci, ct;
+                    if (sum > 64)
+                        (ci, ct) = (64, sum - 64);
+                    else
+                        (ci, ct) = (sum, 0);
+                    set_item(new(ci, sItem.Value.Item, sItem.Value.Type, sItem.Value.Meta));
+                    transfered = ct > 0 ? new(ct, sItem.Value.Item, sItem.Value.Type, sItem.Value.Meta) : null;
+                }
+                else
+                {
+                    var tmp = sItem;
+                    set_item(transfered);
+                    transfered = tmp;
+                }
+                
+                break;
+            case ClickEventButton.Scroll:
+                sItem = get_item();
+                if (transfered.HasValue)
+                    return;
+                if (!sItem.HasValue)
+                    return;
+                sItem = new(64, sItem!.Value.Item, sItem.Value.Type, sItem.Value.Meta);
+                transfered = sItem;
+                break;
+            case ClickEventButton.Right:
+                sItem = get_item();
+                if (!sItem.HasValue && !transfered.HasValue)
+                    return;
+                if(!sItem.HasValue && transfered.HasValue)
+                {
+                    sItem = new(transfered.Value.Item, transfered.Value.Type, transfered.Value.Meta);
+                    set_item(sItem);
+                    transfered = transfered.Value.Count == 1 ? null :
+                        new(transfered.Value.Count - 1, 
+                            transfered.Value.Item, 
+                            transfered.Value.Type, 
+                            transfered.Value.Meta);
+                }
+                else if (sItem.HasValue && transfered.HasValue)
+                {
+                    if (!((sItem?.Item == transfered?.Item) &&
+                          (sItem?.Type == transfered?.Type) &&
+                          (sItem?.Meta == transfered?.Meta)))
+                        return;
+                    if (sItem?.Count >= 64)
+                        return;
 
-        if (id.main)
-        {
-            var inv = playerInventory;
-            var tmp = inv.GetItem(id.id);
-            inv.SetItem(id.id, transferredItem);
-            playerInventory = inv;
-            transferredItem = tmp;
+                    transfered = transfered!.Value.Count == 1 ? null :
+                        new(transfered.Value.Count - 1, transfered.Value.Item, transfered.Value.Type, transfered.Value.Meta);
+                    set_item(new(sItem!.Value.Count + 1, sItem.Value.Item, sItem.Value.Type, sItem.Value.Meta));
+                }
+                else if(sItem.HasValue && !transfered.HasValue)
+                {
+                    var half = sItem.Value.Count % 2 == 0 ? sItem.Value.Count / 2 : (sItem.Value.Count / 2) + 1;
+                    transfered = new(half, sItem.Value.Item, sItem.Value.Type, sItem.Value.Meta);
+                    set_item(new(sItem!.Value.Count - half, sItem.Value.Item, sItem.Value.Type, sItem.Value.Meta));
+                }
+                break;
         }
-        else
+        transferredItem = transfered;
+        playerInventory = inv;
+        toolBar = tools;
+
+        ItemStack? get_item()
         {
-            var tools = toolBar;
-            var tmp = tools[id.id];
-            tools[id.id] = transferredItem;
-            transferredItem = tmp;
-            toolBar = tools;
+            if (id.main)
+                return inv.GetItem(id.id);
+            else
+                return tools[id.id];
+        }
+        void set_item(ItemStack? item)
+        {
+            if (id.main)
+                inv.SetItem(id.id, item);
+            else
+                tools[id.id] = item;
         }
     }
 
