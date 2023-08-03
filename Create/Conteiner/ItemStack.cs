dc0b21d3222d62b5338c75afa86f00e554fb3efa
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2023-08-03 12:42:47 +0200

    Rename to `AreStacksEqual`

--- Create/Conteiner/ItemStack.cs
+++ Create/Conteiner/ItemStack.cs
@@ -117,7 +117,7 @@ public struct ItemStack
     public uint Count => count;
 
     public static bool operator ==(ItemStack a, ItemStack b) =>
-        (a.item == b.item) ? a.Item.IsStackEqual(a, b) : false;
+        (a.item == b.item) ? a.Item.AreStacksEqual(a, b) : false;
     public static bool operator !=(ItemStack a, ItemStack b) => !(a == b);
 
     public override int GetHashCode() => base.GetHashCode();
