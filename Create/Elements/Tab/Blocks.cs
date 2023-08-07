88b450b08d370efa12633fa9b8e3491574b593f5
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2023-08-08 01:43:00 +0200

    Blocks tab

---
+++ Create/Elements/Tab/Blocks.cs
@@ -0,0 +1,17 @@
+﻿using Create.Conteiner;
+
+namespace Create.Elements;
+
+partial class CreativeTabs
+{
+    public static readonly CreativeTab BLOCKS = new()
+    {
+        RegisterElements = () =>
+            (new Block[]
+            {
+                Blocks.STONE,
+                Blocks.DIRT,
+                Blocks.GRASS_BLOCK
+            }).Select(b => new ItemStack(b))
+    };
+}
