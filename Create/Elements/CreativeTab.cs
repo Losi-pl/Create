4e674a2e8dcc46f458f8c736a14fc0fd5900b43a
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2023-08-08 01:42:06 +0200

    Creative Tab's `Icon` & `Items `

--- Create/Elements/CreativeTab.cs
+++ Create/Elements/CreativeTab.cs
@@ -1,14 +1,21 @@
 ﻿using Create.Conteiner;
+using System.Collections.ObjectModel;
 
 namespace Create.Elements;
 
 public class CreativeTab : Baze
 {
     public Func<IEnumerable<ItemStack>>? RegisterElements { private get; init; }
-    ItemStack[]? items;
+    public Func<ItemStack>? CreateIcon { private get; init; }
+    ReadOnlyCollection<ItemStack>? items;
+    ItemStack? icon;
 
     internal void load_stacks()
     {
-        items = (RegisterElements?.Invoke() ?? Enumerable.Empty<ItemStack>()).ToArray();
+        items = new((RegisterElements?.Invoke() ?? Enumerable.Empty<ItemStack>()).ToArray());
+        icon = (CreateIcon?.Invoke() ?? (items.Count > 0 ? items[0] : null));
     }
+
+    public ReadOnlyCollection<ItemStack> Items => items!;
+    public ItemStack? Icon => icon;
 }
