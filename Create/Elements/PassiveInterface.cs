c309a1814e7fad4aa1ace5df99284b1130c370f3
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2023-10-04 18:57:37 +0200

    Class shortning

--- Create/Elements/PassiveInterface.cs
+++ Create/Elements/PassiveInterface.cs
@@ -4,7 +4,4 @@
 /// Czy interfejs pest pasywny i nie możesz z nim wchodzić aktywnie w interakcje 
 /// </summary>
 [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
-public sealed class PassiveInterface : Attribute
-{
-
-}
+public sealed class PassiveInterface : Attribute { }
