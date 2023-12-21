154978e274b7151fed10fc92bfffac0a74cd6c72
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2023-12-22 00:34:31 +0100

    Bazic Class `LogBase`

---
+++ Create/Elements/Bazic/Blocks/LogBase.cs
@@ -0,0 +1,21 @@
+﻿namespace Create.Elements.Bazic.Blocks;
+
+public abstract class LogBase : Block
+{
+    public override bool OnPlaceBlock(PlaceBlock args)
+    {
+        var block = args.BlockStack.AsPlacedBlock();
+        block = new(block.Block, block.Type, args.TargetSide switch
+        {
+            BlockSide.Top => "0",
+            BlockSide.Bottom => "0",
+            BlockSide.East => "1",
+            BlockSide.West => "1",
+            BlockSide.North => "2",
+            BlockSide.South => "2",
+            _ => string.Empty
+        });
+        args.World.SetBlock(args.TargetBlockPozition, block);
+        return true;
+    }
+}
