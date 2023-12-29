2442a917fec87ed23cc2658281238eb3dfb4448b
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2023-12-29 01:25:36 +0100

    Bind Interface Inventory

--- Create/Elements/Bazic/Interfaces/CraftingTableInterface.cs
+++ Create/Elements/Bazic/Interfaces/CraftingTableInterface.cs
@@ -13,13 +13,54 @@ internal class CraftingTableInterface : UserInterface, IUserInterface<CraftingTa
 {
     #nullable disable
     SpacePoint root;
+    InventorySlots inventory;
+    CraftingSlots slots;
     #nullable restore
 
     static (CraftingTableInterface status, SpacePoint point) IUserInterface<CraftingTableInterface>.LoadInterface(InterfaceCreatorArgs args)
     {
         CraftingTableInterface cti = new();
         cti.root = Assets.GetInterface("create:crafting-table");
+        cti.slots = new();
+        cti.inventory = new(Client.TransferedItemSlot,
+            ItemSlot.GetAllSlots(cti.root.Childs.Find("Slots bar", true) ?? new())
+                .Select(s => (s, s.ID ?? 0)),
+            new[] { ItemSlot.GetAllSlots(cti.root.Childs.Find("Row 1", true) ?? new()),
+                    ItemSlot.GetAllSlots(cti.root.Childs.Find("Row 2", true) ?? new()),
+                    ItemSlot.GetAllSlots(cti.root.Childs.Find("Row 3", true) ?? new()) }
+                .SelectMany(r => r)
+                .Select(s => (s, s.ID ?? 0)),
+            ItemSlot.GetAllSlots(cti.root.Childs.Find("Crafting", true) ?? new())
+                .Select(s => (s, s.ID ?? 0)).Where(s => s.Item2 < 9));
+
+        cti.inventory.GetToolBar += () => cti.Player.Entity?.Data.Get("tool_slots") as ToolsBar? ?? new();
+        cti.inventory.SetToolBar += t => cti.Player.Entity?.Data.Set("tool_slots", t);
+
+        cti.inventory.GetTransferredItem += () => cti.Player.Entity?.Data.Get("transferred_item") as ItemStack?;
+        cti.inventory.SetTransferredItem += i => cti.Player.Entity?.Data.Set("transferred_item", i);
+
+        cti.inventory.GetPlayerInventory += () => cti.Player.Entity?.Data.Get("inventory") as PlayerInventory? ?? new();
+        cti.inventory.SetPlayerInventory += t => cti.Player.Entity?.Data.Set("inventory", t);
+
+        cti.inventory.GetContainer += () => cti.slots;
+        cti.inventory.SetContainer += c => cti.slots = c as CraftingSlots ?? new();
 
         return (cti, cti.root)!;
+    }
+
+    public override void Update(UpdateArgs args)
+    {
+        if (!args.activeInventory)
+            return;
+        inventory.UpdateSlotsContent(args.time);
+    }
+
+    class CraftingSlots : IItemContainer
+    {
+        StructArray.Count9<ItemStack?> items;
+
+        public ItemStack? GetItem(int index) => items[index];
+        public void SetItem(int index, ItemStack? item) => items[index] = item;
+        public int Length => 9;
     }
 }
