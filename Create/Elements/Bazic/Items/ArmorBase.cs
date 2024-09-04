c06f05e05eeff65355c69f21de76cbbaad585468
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2024-09-04 20:38:13 +0200

    Base class `ArmorBase`

---
+++ Create/Elements/Bazic/Items/ArmorBase.cs
@@ -0,0 +1,24 @@
+﻿using Create.Conteiner;
+using Create.Space;
+
+namespace Create.Elements.Bazic.Items;
+
+public class ArmorBase : Item
+{
+
+    public virtual Placement GetArmorPlacement(GetArmorPlacementData data) =>
+        Placement.Head | Placement.Torso | Placement.Legs | Placement.Feet;
+
+    public struct GetArmorPlacementData
+    {
+        public ItemStack ItemStack;
+        public LivingEntity Entity;
+        public World World => Entity.Dimention!.World;
+    }
+
+    [Flags]
+    public enum Placement
+    {
+        Head = 1, Torso = 2, Legs = 4, Feet = 8
+    }
+}
