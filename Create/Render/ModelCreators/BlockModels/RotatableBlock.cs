a9716435ac9c86d87507dbb07b44fa8d198f0c07
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2024-09-06 21:37:57 +0200

    Correct a corner

--- Create/Render/ModelCreators/BlockModels/RotatableBlock.cs
+++ Create/Render/ModelCreators/BlockModels/RotatableBlock.cs
@@ -163,22 +163,22 @@ public sealed class RotatableBlock : IBlockModel
             switch (side)
             {
                 case BlockSide.Top:
-                    block_set.pozition.y++;
+                    block_set.pozition = (block_set.pozition.x, block_set.pozition.y + 1, block_set.pozition.z);
                     break;
                 case BlockSide.Bottom:
-                    block_set.pozition.y--;
+                    block_set.pozition = (block_set.pozition.x, block_set.pozition.y - 1, block_set.pozition.z);
                     break;
                 case BlockSide.West:
-                    block_set.pozition.x--;
+                    block_set.pozition = (block_set.pozition.x - 1, block_set.pozition.y, block_set.pozition.z);
                     break;
                 case BlockSide.East:
-                    block_set.pozition.x++;
+                    block_set.pozition = (block_set.pozition.x + 1, block_set.pozition.y, block_set.pozition.z);
                     break;
                 case BlockSide.North:
-                    block_set.pozition.z++;
+                    block_set.pozition = (block_set.pozition.x, block_set.pozition.y, block_set.pozition.z + 1);
                     break;
                 case BlockSide.South:
-                    block_set.pozition.z--;
+                    block_set.pozition = (block_set.pozition.x, block_set.pozition.y, block_set.pozition.z - 1);
                     break;
             }
             PlacedBlock block = args.world.GetBlock(block_set.pozition);
