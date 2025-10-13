6c36489eca85fc6ac2c74d9d72b8e75fd29b3ca3
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2025-10-13 15:09:21 +0200

    Model for vertical slabs

--- Create/Render/ModelCreators/BlockModels/SlabModel.cs
+++ Create/Render/ModelCreators/BlockModels/SlabModel.cs
@@ -50,6 +50,59 @@ public sealed class SlabModel : IBlockModel
                     render_side(BlockSide.Top, info.AsT0.Top, true);
             }
         }
+        else
+        {
+            if (info.AsT1.Column1 is not null)
+            {
+                if (test_side(BlockSide.Bottom))
+                    render_side_v(BlockSide.Bottom, info.AsT1.Column1, false, info.AsT1.IsAlongTheXAxis);
+                if (test_side(BlockSide.Top))
+                    render_side_v(BlockSide.Top, info.AsT1.Column1, false, info.AsT1.IsAlongTheXAxis);
+                if (test_side(BlockSide.West))
+                    render_side_v(BlockSide.West, info.AsT1.Column1, false, info.AsT1.IsAlongTheXAxis);
+                if (test_side(BlockSide.South))
+                    render_side_v(BlockSide.South, info.AsT1.Column1, false, info.AsT1.IsAlongTheXAxis);
+                if (info.AsT1.IsAlongTheXAxis)
+                {
+                    if (info.AsT1.Column2 is null)
+                        render_side_v(BlockSide.North, info.AsT1.Column1, false, info.AsT1.IsAlongTheXAxis);
+                    if (test_side(BlockSide.East))
+                        render_side_v(BlockSide.East, info.AsT1.Column1, false, info.AsT1.IsAlongTheXAxis);
+                }
+                else
+                {
+                    if (info.AsT1.Column2 is null)
+                        render_side_v(BlockSide.East, info.AsT1.Column1, false, info.AsT1.IsAlongTheXAxis);
+                    if (test_side(BlockSide.North))
+                        render_side_v(BlockSide.North, info.AsT1.Column1, false, info.AsT1.IsAlongTheXAxis);
+                }
+            }
+            if (info.AsT1.Column2 is not null)
+            {
+                if (test_side(BlockSide.Bottom))
+                    render_side_v(BlockSide.Bottom, info.AsT1.Column2, true, info.AsT1.IsAlongTheXAxis);
+                if (test_side(BlockSide.Top))
+                    render_side_v(BlockSide.Top, info.AsT1.Column2, true, info.AsT1.IsAlongTheXAxis);
+                if (test_side(BlockSide.East))
+                    render_side_v(BlockSide.East, info.AsT1.Column2, true, info.AsT1.IsAlongTheXAxis);
+                if (test_side(BlockSide.North))
+                    render_side_v(BlockSide.North, info.AsT1.Column2, true, info.AsT1.IsAlongTheXAxis);
+                if (info.AsT1.IsAlongTheXAxis)
+                {
+                    if (info.AsT1.Column1 is null)
+                        render_side_v(BlockSide.South, info.AsT1.Column2, true, info.AsT1.IsAlongTheXAxis);
+                    if (test_side(BlockSide.West))
+                        render_side_v(BlockSide.West, info.AsT1.Column2, true, info.AsT1.IsAlongTheXAxis);
+                }
+                else
+                {
+                    if (info.AsT1.Column1 is null)
+                        render_side_v(BlockSide.West, info.AsT1.Column2, true, info.AsT1.IsAlongTheXAxis);
+                    if (test_side(BlockSide.South))
+                        render_side_v(BlockSide.South, info.AsT1.Column2, true, info.AsT1.IsAlongTheXAxis);
+                }
+            }
+        }
 
         void render_side(BlockSide side, Block block, bool upper)
         {
@@ -73,6 +126,42 @@ public sealed class SlabModel : IBlockModel
                 get_face_uvs_s(uv, upper);
             tex?.RenderSide(constructor, poz, uv, trangles);
         }
+        void render_side_v(BlockSide side, Block block, bool upper, bool along_the_x)
+        {
+            Span<int> trangles = stackalloc int[]
+            {
+                0, 1, 2,
+                3, 2, 1
+            };
+            var tex = block.GetSideTexture(new()
+            {
+                block = args.block,
+                pozition = args.pozition,
+                world = args.world
+            }, side);
+            Span<Vector3> poz = stackalloc Vector3[4];
+            Span<Vector2> uv = stackalloc Vector2[4];
+            get_face_pozitions_v(poz, side, upper, along_the_x);
+            if(along_the_x)
+            {
+                if (side is BlockSide.North or BlockSide.South)
+                    get_face_uvs(uv);
+                else if(side is BlockSide.Top or BlockSide.Bottom)
+                    get_face_uvs_s(uv, side is BlockSide.Top ? upper : !upper);
+                else
+                    get_face_uvs_s_v(uv, side is BlockSide.East ? upper : !upper);
+            }
+            else
+            {
+                if (side is BlockSide.East or BlockSide.West)
+                    get_face_uvs(uv);
+                else if (side is BlockSide.Top or BlockSide.Bottom)
+                    get_face_uvs_s_v(uv, side is BlockSide.Top ? upper : !upper);
+                else
+                    get_face_uvs_s_v(uv, side is BlockSide.South ? upper : !upper);
+            }
+            tex?.RenderSide(constructor, poz, uv, trangles);
+        }
         void get_face_uvs(Span<Vector2> uvs)
         {
             uvs[0] = new Vector2(0, 1);
@@ -89,6 +178,16 @@ public sealed class SlabModel : IBlockModel
             if (upper)
                 for (int i = 0; i < 4; i++)
                     uvs[i] = uvs[i] + new Vector2(0, .5f);
+        }
+        void get_face_uvs_s_v(Span<Vector2> uvs, bool upper)
+        {
+            uvs[0] = new Vector2(0, 1);
+            uvs[1] = new Vector2(.5f, 1);
+            uvs[2] = new Vector2(0, 0);
+            uvs[3] = new Vector2(.5f, 0);
+            if (upper)
+                for (int i = 0; i < 4; i++)
+                    uvs[i] += new Vector2(.5f, 0);
         }
         void get_face_pozitions(Span<Vector3> pozitions, BlockSide side, bool upper)
         {
@@ -135,6 +234,65 @@ public sealed class SlabModel : IBlockModel
             if (upper)
                 for (int i = 0; i < 4; i++)
                     pozitions[i] = pozitions[i] + new Vector3(0, .5f, 0);
+        }
+        void get_face_pozitions_v(Span<Vector3> pozitions, BlockSide side, bool upper, bool along_the_x)
+        {
+            Span<Vector3> poz = stackalloc Vector3[4];
+            Vector3 bl_poz = args.pozition.ToVector();
+            switch (side)
+            {
+                case BlockSide.North:
+                    poz[0] = new Vector3(1, 1, 1);
+                    poz[1] = new Vector3(0, 1, 1);
+                    poz[2] = new Vector3(1, 0, 1);
+                    poz[3] = new Vector3(0, 0, 1);
+                    break;
+                case BlockSide.South:
+                    poz[0] = new Vector3(0, 1, 0);
+                    poz[1] = new Vector3(1, 1, 0);
+                    poz[2] = new Vector3(0, 0, 0);
+                    poz[3] = new Vector3(1, 0, 0);
+                    break;
+                case BlockSide.East:
+                    poz[0] = new Vector3(1, 1, 0);
+                    poz[1] = new Vector3(1, 1, 1);
+                    poz[2] = new Vector3(1, 0, 0);
+                    poz[3] = new Vector3(1, 0, 1);
+                    break;
+                case BlockSide.West:
+                    poz[0] = new Vector3(0, 1, 1);
+                    poz[1] = new Vector3(0, 1, 0);
+                    poz[2] = new Vector3(0, 0, 1);
+                    poz[3] = new Vector3(0, 0, 0);
+                    break;
+                case BlockSide.Top:
+                    poz[0] = new Vector3(0, 1, 1);
+                    poz[1] = new Vector3(1, 1, 1);
+                    poz[2] = new Vector3(0, 1, 0);
+                    poz[3] = new Vector3(1, 1, 0);
+                    break;
+                case BlockSide.Bottom:
+                    poz[0] = new Vector3(0, 0, 0);
+                    poz[1] = new Vector3(1, 0, 0);
+                    poz[2] = new Vector3(0, 0, 1);
+                    poz[3] = new Vector3(1, 0, 1);
+                    break;
+            }
+
+            if (along_the_x)
+                for (int i = 0; i < 4; i++)
+                    pozitions[i] = bl_poz + (poz[i] * new Vector3(1, 1, .5f));
+            else
+                for (int i = 0; i < 4; i++)
+                    pozitions[i] = bl_poz + (poz[i] * new Vector3(.5f, 1, 1));
+
+            if (upper)
+                if(along_the_x)
+                    for (int i = 0; i < 4; i++)
+                        pozitions[i] = pozitions[i] + new Vector3(0, 0, .5f);
+                else
+                    for (int i = 0; i < 4; i++)
+                        pozitions[i] = pozitions[i] + new Vector3(.5f, 0, 0);
         }
         bool test_side(BlockSide side)
         {
