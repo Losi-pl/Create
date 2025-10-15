ba48ea8f84e03a6c3f64038c5905df9a2cbf6f46
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2025-10-15 21:26:32 +0200

    Block `create:oak-planks-stairs`

--- Create/Elements/Blocks.cs
+++ Create/Elements/Blocks.cs
@@ -20,6 +20,7 @@ public static class Blocks
     public static readonly Block STONE_SLAB = new Stone.Slab();
     public static readonly Block STONE_STAIRS = new Stone.Stairs();
     public static readonly Block OAK_PLANKS_SLAB = new OakPlanks.Slab();
+    public static readonly Block OAK_PLANKS_STAIRS = new OakPlanks.Stairs();
     public static readonly Block DEBUG_BLOCK = new DebugBlock();
     public static readonly Block DEBUG_SLAB = new DebugBlock.Slab();
 }
