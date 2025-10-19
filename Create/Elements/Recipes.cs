5af149b12b6380222f7ccaf0d5f59c40a982c1e7
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2025-10-19 22:40:24 +0200

    Recipe for `create:debug-slab`

--- Create/Elements/Recipes.cs
+++ Create/Elements/Recipes.cs
@@ -13,10 +13,13 @@ partial interface IRecipe
             "pp",
             ('p', new(Blocks.OAK_PLANKS))
           ));
+
         mod.RegisterRecipe("stone-slab", new Standard(new(6, Blocks.STONE_SLAB),
             "sss", ('s', new(Blocks.STONE)) ));
         mod.RegisterRecipe("oak-planks-slab", new Standard(new(6, Blocks.OAK_PLANKS_SLAB),
             "ooo", ('o', new(Blocks.OAK_PLANKS)) ));
+        mod.RegisterRecipe("debug-slab", new Standard(new(6, Blocks.DEBUG_SLAB),
+            "ddd", ('d', new(Blocks.DEBUG_BLOCK))));
 
         Bazic.Blocks.SlabBase.BazicSlabRecipes(mod);
     }
