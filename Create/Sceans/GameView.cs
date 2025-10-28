78bb46a435986b6ec655d81b8f544cc0b39e3bab
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2025-10-28 07:46:11 +0000

    Rename method to `RotateCameraUpdate` and shortening code

--- Create/Sceans/GameView.cs
+++ Create/Sceans/GameView.cs
@@ -115,7 +115,7 @@ internal sealed partial class GameView : Scean
         }
 
         if (Mouse.Lock)
-            camera_rotation();
+            RotateCameraUpdate();
 
         if (Keyboard.Escape.Down)
         {
@@ -230,27 +230,16 @@ internal sealed partial class GameView : Scean
         terrain.ChunkUpdate(args.Time);
         Client.Me.Entity!.Data.Set("inventory_open", inventory);
         
-        void camera_rotation()
+        void RotateCameraUpdate()
         {
-            Vector2 rot;
-            {
-                var rot_v = Client.Me.Entity!.Data.Get("camera_rot");
-                rot = rot_v != null ? (Vector2)rot_v : new();
-            }
+            var rot = Client.Me.Entity!.Data.Get("camera_rot") as Vector2? ?? new();
             
             var rota_d = Mouse.Delta;
             rot.X += (float)(rota_d.x * args.Time) * 3;
             rot.Y -= (float)(rota_d.y * args.Time) * 3;
 
-            if (rot.Y < -90f)
-                rot.Y = -90f;
-            if (rot.Y > 90f)
-                rot.Y = 90f;
-
-            if (rot.X < 0)
-                rot.X = 360 + rot.X;
-            if (rot.X > 360)
-                rot.X = rot.X - 360;
+            rot.Y = Math.Clamp(rot.Y, -90f, 90f);
+            rot.X = MathC.InSection(rot.X, 0, 360);
 
             camera.Rotation = new(rot.Y, rot.X, 0);
             Client.Me.Entity!.Data.Set("camera_rot", rot);
