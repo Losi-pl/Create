cac516e735799975aad4cb7229435fec785f1ccc
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2026-05-12 23:10:53 +0200

    Two more extra library's

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module com.losi.create {
     requires org.lwjgl;
     requires steamworks4j;
     requires org.joml.primitives;
+    requires static org.jetbrains.annotations;
+    requires koloboke.api.jdk8;
     requires nbt.querz;
 
     exports com.losi.create;
