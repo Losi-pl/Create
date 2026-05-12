c8f9574dd207b2f0d449bcf52443bdd6e9817858
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2026-05-12 21:17:26 +0200

    Add add `module-info.java`

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.losi.create {
+    requires org.joml;
+    requires org.lwjgl;
+    requires steamworks4j;
+    requires org.joml.primitives;
+    requires nbt.querz;
+
+    exports com.losi.create;
+}
