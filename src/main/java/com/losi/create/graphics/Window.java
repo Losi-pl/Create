cbd7a3a2d48c2963894f998102629c8ba9e19a3b
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2026-05-15 16:06:18 +0200

    Window resync & No more warnings

--- src/main/java/com/losi/create/graphics/Window.java
+++ src/main/java/com/losi/create/graphics/Window.java
@@ -4,20 +4,27 @@ import org.joml.Vector2i;
 import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
 
+import java.lang.ref.Cleaner;
+
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
+@SuppressWarnings("unused")
 public class Window {
     private static final ThreadLocal<Window> currentWindow = new ThreadLocal<>();
+    private static final Cleaner cleaner = Cleaner.create();
     private static boolean initialized = false;
 
-    private transient long window = NULL;
-    private boolean thread_bound = false;
-    private String title = null;
-    private Vector2i size = null;
-    private Monitor monitor = null;
-    private boolean vSync = false;
+    private transient final Object sync = new Object();
+    private transient volatile long window = NULL;
+    @SuppressWarnings({"FieldCanBeLocal"})
+    private transient Cleaner.Cleanable handleDestroyer;
+    private volatile boolean thread_bound = false;
+    private volatile String title = null;
+    private volatile Vector2i size = null;
+    private volatile Monitor monitor = null;
+    private volatile boolean vSync = false;
 
     public Window()
     {
@@ -29,13 +36,21 @@ public class Window {
         }
     }
 
+    public String title() { return title; }
+    public String title(String _new) {
+        title = _new;
+        if(window != NULL)
+            glfwSetWindowTitle(window, title == null ? "null" : title);
+        return title;
+    }
+
+    public boolean vSync() { return vSync; }
+    public boolean vSync(boolean _new) { vSync = _new; return vSync; }
+
     private void initGL() {
         GLFWErrorCallback.createPrint(System.err).set();
         if ( !glfwInit() ) throw new IllegalStateException("Unable to initialize GLFW");
     }
-
-    /*
-    }*/
 
     public void run() {
         glfwShowWindow(window);
@@ -51,34 +66,44 @@ public class Window {
 
     public void create()
     {
-        glfwDefaultWindowHints();
-        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
-
-        if(monitor == null)
-            monitor = Monitor.list().getFirst();
-        if(size == null)
-            size = new Vector2i(monitor.width() * 2 / 3, monitor.height() * 2 / 3);
-
-        window = glfwCreateWindow(size.x, size.y, title == null ? "null" : title, NULL, NULL);
-        if ( window == NULL )
-            throw new RuntimeException("Failed to create the GLFW window");
-        else
+        synchronized (sync)
         {
-            var pos = monitor.position();
-            var work = monitor.workArea();
+            if(window != NULL)
+                return;
 
-            int windowX = pos.x() + (work.width() - size.x) / 2;
-            int windowY = pos.y() + (work.height() - size.y) / 2;
+            glfwDefaultWindowHints();
+            glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+            glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
 
-            glfwSetWindowPos(window, windowX, windowY);
+            if(monitor == null)
+                monitor = Monitor.list().getFirst();
+            if(size == null)
+                size = new Vector2i(monitor.width() * 2 / 3, monitor.height() * 2 / 3);
+
+            window = glfwCreateWindow(size.x, size.y, title == null ? "null" : title, NULL, NULL);
+            if ( window == NULL )
+                throw new RuntimeException("Failed to create the GLFW window");
         }
 
-        /*// Setup a key callback. It will be called every time a key is pressed, repeated or released.
-        glfwSetKeyCallback(window, (wind, key, scancode, action, mods) -> {
+        final var handler = window;
+        handleDestroyer = cleaner.register(this, () -> glfwDestroyWindow(handler));
+
+        var pos = monitor.position();
+        var work = monitor.workArea();
+        glfwSetWindowPos(window,
+                pos.x() + (work.width() - size.x) / 2,
+                pos.y() + (work.height() - size.y) / 2);
+
+        /*// glfwSetKeyCallback(window, (wind, key, scancode, action, mods) -> {
             if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
                 glfwSetWindowShouldClose(wind, true); // We will detect this in the rendering loop
         });*/
+    }
+
+    public void close()
+    {
+        if (window != NULL)
+            glfwWindowShouldClose(window);
     }
 
     public void threadBind()
