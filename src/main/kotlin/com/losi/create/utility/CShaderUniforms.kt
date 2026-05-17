7f9f88c8c5e61e9f8552ce351dd56833fdfc6fee
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2026-05-18 00:49:46 +0200

    Cut V2.0

--- src/main/kotlin/com/losi/create/utility/CShaderUniforms.kt
+++ src/main/kotlin/com/losi/create/utility/CShaderUniforms.kt
@@ -1,33 +1,20 @@
+@file:JvmName("CShaderUniforms")
 @file:Suppress("SpellCheckingInspection")
 
 package com.losi.create.utility
 
-import org.jetbrains.annotations.NotNull
 import org.joml.Matrix4f
 import org.lwjgl.opengl.GL20.*
 import org.lwjgl.system.MemoryStack
 import org.lwjgl.system.NativeType
 
-class CShaderUniforms
+fun glUniformMatrix4fv(@NativeType("GLint")location: Int,
+                       @NativeType("GLboolean")transpose: Boolean,
+                       @NativeType("GLfloat const *") matrix: Matrix4f)
 {
-    companion object
-    {
-        @JvmStatic
-        fun glUniformMatrix4fv(@NativeType("GLint")location: Int,
-                               @NativeType("GLboolean")transpose: Boolean,
-                               @NativeType("GLfloat const *") @NotNull matrix: Matrix4f)
-        {
-            MemoryStack.stackPush().use {
-                val buff = it.mallocFloat(4 * 4)
-                buff.put(matrix.m00()).put(matrix.m01()).put(matrix.m02()).put(matrix.m03())
-                    .put(matrix.m10()).put(matrix.m11()).put(matrix.m12()).put(matrix.m13())
-                    .put(matrix.m20()).put(matrix.m21()).put(matrix.m22()).put(matrix.m23())
-                    .put(matrix.m30()).put(matrix.m31()).put(matrix.m32()).put(matrix.m33())
-                buff.flip()
-                glUniformMatrix4fv(location, transpose, buff)
-            }
-        }
+    MemoryStack.stackPush().use {
+        val buff = it.mallocFloat(4 * 4)
+        matrix.get(buff)
+        glUniformMatrix4fv(location, transpose, buff)
     }
-
 }
-
