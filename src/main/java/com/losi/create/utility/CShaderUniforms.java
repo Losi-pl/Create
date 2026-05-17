7f5db9dc72952da70da35edb5635e638ff251efc
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2026-05-18 01:17:41 +0200

    Give up.

---
+++ src/main/java/com/losi/create/utility/CShaderUniforms.java
@@ -0,0 +1,24 @@
+package com.losi.create.utility;
+
+import org.jetbrains.annotations.NotNull;
+import org.joml.Matrix4f;
+import org.lwjgl.system.MemoryStack;
+import org.lwjgl.system.NativeType;
+
+import static org.lwjgl.opengl.GL20.*;
+
+@SuppressWarnings({"SpellCheckingInspection", "typo"})
+public class CShaderUniforms {
+    public static void glUniformMatrix4f(
+            @NativeType("GLint") int location,
+            @NativeType("GLboolean")boolean transpose,
+            @NativeType("GLfloat const *") @NotNull Matrix4f matrix)
+    {
+        try (var stack = MemoryStack.stackPush())
+        {
+            var buff = stack.mallocFloat(4 * 4);
+            matrix.get(buff);
+            glUniformMatrix4fv(location, transpose, buff);
+        }
+    }
+}
