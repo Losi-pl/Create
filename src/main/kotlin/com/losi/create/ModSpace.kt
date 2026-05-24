57211cdd0c4ce159bc03ca277c82b16536261c3b
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2026-05-24 10:56:26 +0200

    Shorten

--- src/main/kotlin/com/losi/create/ModSpace.kt
+++ src/main/kotlin/com/losi/create/ModSpace.kt
@@ -2,21 +2,11 @@ package com.losi.create
 
 import com.losi.create.assets.ResourceSpace
 
-class ModSpace {
+@ConsistentCopyVisibility
+data class ModSpace internal constructor(val version: String, val identity: String, val resourceSpace: ResourceSpace = ResourceSpace(), val abstract: Boolean = false) {
     companion object
     {
         val modules: Map<String, ModSpace> = mapOf("create" to ModSpace(Version.version, "create"))
-
-    }
-
-    val version: String
-    val identity: String
-    val resourceSpace: ResourceSpace
-
-    internal constructor(version: String, identity: String) {
-        this.version = version
-        this.identity = identity
-        resourceSpace = ResourceSpace()
     }
 
     override fun toString(): String = "Mod: $identity"
