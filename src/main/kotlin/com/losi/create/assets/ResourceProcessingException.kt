06b8bc87d4b3aa24307f87d3e8f9e182f715b8c3
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2026-05-28 21:37:50 +0200

    Documentation `ResourceProcessingException`, `ResourceSpace`

--- src/main/kotlin/com/losi/create/assets/ResourceProcessingException.kt
+++ src/main/kotlin/com/losi/create/assets/ResourceProcessingException.kt
@@ -1,3 +1,4 @@
 package com.losi.create.assets
 
+/**This exception is used when there is a problem with the data provided to process the resources*/
 class ResourceProcessingException(message: String, cause: Throwable? = null) : RuntimeException(message, cause)
