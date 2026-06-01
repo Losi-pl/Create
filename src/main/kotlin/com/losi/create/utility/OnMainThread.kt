fad09ade3b93579cb6a967046deb8776f35b2e5b
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2026-06-01 21:02:33 +0200

    Documentation `OnMainThread`

--- src/main/kotlin/com/losi/create/utility/OnMainThread.kt
+++ src/main/kotlin/com/losi/create/utility/OnMainThread.kt
@@ -3,25 +3,33 @@ package com.losi.create.utility
 import java.util.concurrent.ConcurrentLinkedQueue
 import java.util.concurrent.CountDownLatch
 
+/**Operations performed on the main thread of the application*/
 object OnMainThread
 {
+    /**The queue of methods to me performed in the main thread*/
     private val queue: ConcurrentLinkedQueue<Runnable> = ConcurrentLinkedQueue()
+    /**Main [Thread] of the application*/
     internal var mainThread: Thread? = null
 
-    internal fun callAction(@Suppress("unused") ignore: Float)
-    {
+    /**In this method lambdas from the [queue] are actually called
+     * Loops through the queue until it's empty*/
+    internal fun callAction(@Suppress("unused") ignore: Float) {
         while (true) {
             val item = queue.poll() ?: break
             item.run()
         }
     }
-
+    /**Test if the caller of the method in calling from the main thread*/
     @JvmStatic
     fun isMain(): Boolean = mainThread == Thread.currentThread()
-
+    /**Registers a lambda to be executed in the main thread
+     *
+     * Will not wait for the lambda to be finished*/
     @JvmStatic
     fun schedule(action: Runnable) = queue.offer(action)
-
+    /**Registers a lambda to be executed in the main thread
+     *
+     * Will wait until the lambda is done before continuing*/
     @JvmStatic @Suppress("unused")
     inline fun <T> query(crossinline action: () -> T): T
     {
