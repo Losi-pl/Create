e50ea9683dcfc43b03c9ab0f2bdc2aeed2fe3910
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2026-06-01 21:02:23 +0200

    Documentation `ExpandedConsumer`

--- src/main/kotlin/com/losi/create/utility/ExpandedConsumer.kt
+++ src/main/kotlin/com/losi/create/utility/ExpandedConsumer.kt
@@ -2,11 +2,17 @@ package com.losi.create.utility
 
 import java.util.function.Consumer
 
+/**A collection of lambdas to be called collectively at once*/
 class ExpandedConsumer<T> : Consumer<T>
 {
+    /**For asynchronous support*/
     private var sync = Any()
+    /**All registered lambdas*/
     private var consumers: List<Consumer<T>> = mutableListOf<Consumer<T>>().calcify()
 
+    /**Used to add a new lambda to the collection
+     *
+     * TODO: Make the method return it's lambda*/
     fun add(runnable: Consumer<T>) {
         synchronized(sync) {
             val temp = ArrayList(consumers)
@@ -14,7 +20,8 @@ class ExpandedConsumer<T> : Consumer<T>
             consumers = temp.calcify()
         }
     }
-
+    /**Removes a specific lambda from the collection
+     * @return `true` if lambda was successfully removed*/
     @Suppress("UNUSED")
     fun remove(runnable: Consumer<T>): Boolean {
         synchronized(sync) {
