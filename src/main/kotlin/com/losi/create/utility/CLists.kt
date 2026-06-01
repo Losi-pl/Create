08823147f38a42d5787537e1b7a82f18f331ad05
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2026-06-01 21:02:09 +0200

    Documentation for utility's

--- src/main/kotlin/com/losi/create/utility/CLists.kt
+++ src/main/kotlin/com/losi/create/utility/CLists.kt
@@ -3,5 +3,7 @@ package com.losi.create.utility
 
 import java.util.Collections
 
+/**Creates a reflection of a list that is read only but still reflects changes to the original*/
 fun <T> List<T>.calcify(): List<T> = Collections.unmodifiableList(this)
+/**Creates a reflection of a map that is read only but still reflects changes to the original*/
 fun <T> Map<String, T>.calcify(): Map<String, T> = Collections.unmodifiableMap(this)
--- src/main/kotlin/com/losi/create/utility/COverloads.kt
+++ src/main/kotlin/com/losi/create/utility/COverloads.kt
@@ -2,4 +2,8 @@ package com.losi.create.utility
 
 import com.koloboke.collect.set.LongSet
 
+/**Adds the [value] to the set
+ *
+ * Apparently the interface has two methods that in the Kotlin interpretation come down to the same thing but
+ * instead of just choosing one over the other, Kotlin will refuse to choose one and return a compilation error.*/
 internal fun LongSet.forceAdd(value: Long) = COverloads.add(this, value)
