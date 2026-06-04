9d122a1a687658409dfef47f4b0ba43de939257d
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2026-06-04 13:52:08 +0200

    Enum `GLTextureWrappingMode`

---
+++ src/main/kotlin/com/losi/create/graphics/gl/GLTextureWrappingMode.kt
@@ -0,0 +1,23 @@
+package com.losi.create.graphics.gl
+
+import org.lwjgl.opengl.GL20.*
+
+/**The mode of texture wrapping then exiting the border*/
+enum class GLTextureWrappingMode(val gl: Int) {
+    /**Repeats the texture.*/
+    Repeat(GL_REPEAT),
+    /**Repeats the texture, but mirrored with odd coordinates.*/
+    MirroredRepeat(GL_MIRRORED_REPEAT),
+    /**Clamps the coordinates between `0.0` and `1.0`.*/
+    ClampToEdge(GL_CLAMP_TO_EDGE),
+    /**Will give the coordinates outside of `0.0` and `1.0` a specified border color.*/
+    ClampToBorder(GL_CLAMP_TO_BORDER)
+    ;
+
+    companion object {
+        /**Factory to get the format by name if needed,
+         * or to validate if a constant is supported.
+         */
+        fun of(gl: Int) = GLTextureWrappingMode.entries.find { it.gl == gl }
+    }
+}
