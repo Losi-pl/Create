06134028a647a7a50f2015fb759c541a32368fa8
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2026-06-06 19:41:35 +0200

    Move `GL21` file

--- src/main/kotlin/com/losi/create/graphics/gl/wrapper/GL21.kt
+++ src/main/kotlin/com/losi/create/graphics/gl/wrapper/GL21.kt
@@ -12,19 +12,19 @@ public class GL21Cr extends GL20Cr {
     /** {@code void glUniformMatrix4x3fv(GLint location, GLsizei count, GLboolean transpose, GLfloat const * value)} */
     public static void glUniformMatrix4x3f(@NativeType("GLint") int location, @NativeType("GLboolean")boolean transpose, @NativeType("GLfloat const *") @NotNull Matrix4x3f matrix) {
         try (var stack = MemoryStack.stackPush())
-        {
-            var buff = stack.mallocFloat(4 * 3);
-            matrix.get(buff);
-            glUniformMatrix4x3fv(location, transpose, buff);
+            {
+                var buff = stack.mallocFloat(4 * 3);
+                matrix.get(buff);
+                glUniformMatrix4x3fv(location, transpose, buff);
+            }
         }
-    }
     /** {@code void glUniformMatrix3x2fv(GLint location, GLsizei count, GLboolean transpose, GLfloat const * value)} */
     public static void glUniformMatrix3x2f( @NativeType("GLint") int location, @NativeType("GLboolean")boolean transpose, @NativeType("GLfloat const *") @NotNull Matrix3x2f matrix) {
         try (var stack = MemoryStack.stackPush())
-        {
-            var buff = stack.mallocFloat(3 * 2);
-            matrix.get(buff);
-            glUniformMatrix3x2fv(location, transpose, buff);
+            {
+                var buff = stack.mallocFloat(3 * 2);
+                matrix.get(buff);
+                glUniformMatrix3x2fv(location, transpose, buff);
+            }
         }
-    }
 }
