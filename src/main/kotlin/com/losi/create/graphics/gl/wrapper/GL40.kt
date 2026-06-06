d41975866972ddb5ef9902b456e632cac622d8be
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2026-06-06 19:42:17 +0200

    Move `GL40` file

--- src/main/kotlin/com/losi/create/graphics/gl/wrapper/GL40.kt
+++ src/main/kotlin/com/losi/create/graphics/gl/wrapper/GL40.kt
@@ -11,47 +11,47 @@ public class GL40Cr extends GL21Cr {
     /** {@code void glUniformMatrix4dv(GLint location, GLsizei count, GLboolean transpose, GLdouble const * value)} */
     public static void glUniformMatrix4d(@NativeType("GLint") int location, @NativeType("GLboolean")boolean transpose, @NativeType("GLdouble const *") @NotNull Matrix4d matrix) {
         try (var stack = MemoryStack.stackPush())
-        {
-            var buff = stack.mallocDouble(4 * 4);
-            matrix.get(buff);
-            glUniformMatrix4dv(location, transpose, buff);
+            {
+                var buff = stack.mallocDouble(4 * 4);
+                matrix.get(buff);
+                glUniformMatrix4dv(location, transpose, buff);
+            }
         }
-    }
     /** {@code void glUniformMatrix3fv(GLint location, GLsizei count, GLboolean transpose, GLdouble const * value)} */
     public static void glUniformMatrix3d(@NativeType("GLint") int location, @NativeType("GLboolean")boolean transpose, @NativeType("GLdouble const *") @NotNull Matrix3d matrix) {
         try (var stack = MemoryStack.stackPush())
-        {
-            var buff = stack.mallocDouble(3 * 3);
-            matrix.get(buff);
-            glUniformMatrix3dv(location, transpose, buff);
+            {
+                var buff = stack.mallocDouble(3 * 3);
+                matrix.get(buff);
+                glUniformMatrix3dv(location, transpose, buff);
+            }
         }
-    }
     /** {@code void glUniformMatrix2fv(GLint location, GLsizei count, GLboolean transpose, GLdouble const * value)} */
     public static void glUniformMatrix2d(@NativeType("GLint") int location, @NativeType("GLboolean")boolean transpose, @NativeType("GLdouble const *") @NotNull Matrix2d matrix) {
         try (var stack = MemoryStack.stackPush())
-        {
-            var buff = stack.mallocDouble(2 * 2);
-            matrix.get(buff);
-            glUniformMatrix2dv(location, transpose, buff);
+            {
+                var buff = stack.mallocDouble(2 * 2);
+                matrix.get(buff);
+                glUniformMatrix2dv(location, transpose, buff);
+            }
         }
-    }
 
     /** {@code void glUniformMatrix4x3dv(GLint location, GLsizei count, GLboolean transpose, GLdouble const * value)} */
     public static void glUniformMatrix4x3d(@NativeType("GLint") int location, @NativeType("GLboolean")boolean transpose, @NativeType("GLdouble const *") @NotNull Matrix4x3d matrix) {
         try (var stack = MemoryStack.stackPush())
-        {
-            var buff = stack.mallocDouble(4 * 3);
-            matrix.get(buff);
-            glUniformMatrix4x3dv(location, transpose, buff);
+            {
+                var buff = stack.mallocDouble(4 * 3);
+                matrix.get(buff);
+                glUniformMatrix4x3dv(location, transpose, buff);
+            }
         }
-    }
     /** {@code void glUniformMatrix3x2fv(GLint location, GLsizei count, GLboolean transpose, GLdouble const * value)} */
     public static void glUniformMatrix3x2d(@NativeType("GLint") int location, @NativeType("GLboolean")boolean transpose, @NativeType("GLdouble const *") @NotNull Matrix3x2d matrix) {
         try (var stack = MemoryStack.stackPush())
-        {
-            var buff = stack.mallocDouble(3 * 2);
-            matrix.get(buff);
-            glUniformMatrix3x2dv(location, transpose, buff);
+            {
+                var buff = stack.mallocDouble(3 * 2);
+                matrix.get(buff);
+                glUniformMatrix3x2dv(location, transpose, buff);
+            }
         }
-    }
 }
