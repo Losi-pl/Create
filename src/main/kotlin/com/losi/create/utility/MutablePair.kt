97bb6875b839f0917ae4a7e0bb0f6885627cbc18
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2026-06-06 15:07:40 +0200

    Something?

--- src/main/kotlin/com/losi/create/utility/MutablePair.kt
+++ src/main/kotlin/com/losi/create/utility/MutablePair.kt
@@ -2,7 +2,13 @@ package com.losi.create.utility
 
 import java.io.Serializable
 
-data class MutablePair<A, B>(var first: A, var second: B) : Serializable
-{
+data class MutablePair<out A, out B>(
+    var first: @UnsafeVariance A,
+    var second: @UnsafeVariance B) : Serializable {
+
+    /**
+     * Returns string representation of the [MutablePair] including its [first] and [second] values.
+     */
     override fun toString() = "($first, $second)"
 }
+
