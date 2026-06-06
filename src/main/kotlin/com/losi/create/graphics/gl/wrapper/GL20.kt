de4065e1d88313a02a98511f1ea75600a9d844d6
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2026-06-06 19:40:43 +0200

    Move `GL20` file

--- src/main/kotlin/com/losi/create/graphics/gl/wrapper/GL20.kt
+++ src/main/kotlin/com/losi/create/graphics/gl/wrapper/GL20.kt
@@ -12,28 +12,28 @@ public class GL20Cr {
     /** {@code void glUniformMatrix4fv(GLint location, GLsizei count, GLboolean transpose, GLfloat const * value)} */
     public static void glUniformMatrix4f(@NativeType("GLint") int location, @NativeType("GLboolean")boolean transpose, @NativeType("GLfloat const *") @NotNull Matrix4f matrix) {
         try (var stack = MemoryStack.stackPush())
-        {
-            var buff = stack.mallocFloat(4 * 4);
-            matrix.get(buff);
-            glUniformMatrix4fv(location, transpose, buff);
+            {
+                var buff = stack.mallocFloat(4 * 4);
+                matrix.get(buff);
+                glUniformMatrix4fv(location, transpose, buff);
+            }
         }
-    }
     /** {@code void glUniformMatrix3fv(GLint location, GLsizei count, GLboolean transpose, GLfloat const * value)} */
     public static void glUniformMatrix3f(@NativeType("GLint") int location, @NativeType("GLboolean")boolean transpose, @NativeType("GLfloat const *") @NotNull Matrix3f matrix) {
         try (var stack = MemoryStack.stackPush())
-        {
-            var buff = stack.mallocFloat(3 * 3);
-            matrix.get(buff);
-            glUniformMatrix3fv(location, transpose, buff);
+            {
+                var buff = stack.mallocFloat(3 * 3);
+                matrix.get(buff);
+                glUniformMatrix3fv(location, transpose, buff);
+            }
         }
-    }
     /** {@code void glUniformMatrix2fv(GLint location, GLsizei count, GLboolean transpose, GLfloat const * value)} */
     public static void glUniformMatrix2f(@NativeType("GLint") int location, @NativeType("GLboolean")boolean transpose, @NativeType("GLfloat const *") @NotNull Matrix2f matrix) {
         try (var stack = MemoryStack.stackPush())
-        {
-            var buff = stack.mallocFloat(2 * 2);
-            matrix.get(buff);
-            glUniformMatrix2fv(location, transpose, buff);
+            {
+                var buff = stack.mallocFloat(2 * 2);
+                matrix.get(buff);
+                glUniformMatrix2fv(location, transpose, buff);
+            }
         }
-    }
 }
