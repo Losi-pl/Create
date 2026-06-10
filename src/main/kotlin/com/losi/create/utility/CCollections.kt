2b819fc4bc3952ff19014c6ad109395964c5fe88
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2026-06-11 01:43:26 +0200

    Move over to `Collections`

--- src/main/kotlin/com/losi/create/utility/CCollections.kt
+++ src/main/kotlin/com/losi/create/utility/CCollections.kt
@@ -5,5 +5,76 @@ import java.util.Collections
 
 /**Creates a reflection of a list that is read only but still reflects changes to the original*/
 fun <T> List<T>.calcify(): List<T> = Collections.unmodifiableList(this)
+
 /**Creates a reflection of a map that is read only but still reflects changes to the original*/
 fun <T> Map<String, T>.calcify(): Map<String, T> = Collections.unmodifiableMap(this)
+
+/**A variation of [chunked] but reusing the same list to avoid extra allocation*/
+fun <T> Sequence<T>.chunkedReuse(size: Int) = sequence {
+    require(size > 0) { "Chunk size must be positive" }
+    val buffer = mutableListOf<T>()
+    var i = 0
+    for (item in this@chunkedReuse) {
+        if (i == size) { yield(buffer); i = 0 }
+
+        if(buffer.size == i)
+            buffer.add(item)
+        else buffer[i] = item
+        ++i
+    }
+    if (i > 0) {
+        for(j in i until size)
+            buffer.removeLast()
+        yield(buffer)
+    }
+}
+
+/**Finds and returns the first element in the [Map] that meats the [condition], if nothing is found will return `null`*/
+inline fun <K, F> Map<K, F>.findFirst(condition: (Map.Entry<K, F>) -> Boolean): Map.Entry<K, F>? {
+    this.forEach {
+        if(condition(it))
+            return it
+    }
+    return null
+}
+
+/**Goes through the list ensuring that all elements in it are equal to each other
+ * @return First value of the list, if not all values are equal returns [negative]*/
+inline fun <T: Comparable<T>> List<T>.assertAllEqual(negative: () -> T): T {
+    if(this.isEmpty())
+        return negative()
+    val c: T = this[0]
+    this.forEach { if(it.compareTo(c) != 0) return it }
+    return negative()
+}
+
+/**Goes through the array ensuring that all elements in it are equal to each other
+ * @return First value of the array, if not all values are equal returns [negative]*/
+inline fun <T: Comparable<T>> Array<T>.assertAllEqual(negative: () -> T): T {
+    if(this.isEmpty())
+        return negative()
+    val c: T = this[0]
+    this.forEach { if(it.compareTo(c) != 0) return it }
+    return negative()
+}
+
+/**Goes through the sequence ensuring that all elements in it are equal to each other
+ * @return First value of the sequence, if not all values are equal returns [negative]*/
+inline fun <T: Comparable<T>> Sequence<T>.assertAllEqual(negative: () -> T): T {
+    val it = this.iterator()
+    if(!it.hasNext())
+        return negative()
+    val c: T = it.next()
+    while (it.hasNext())
+        if(c.compareTo(it.next()) != 0)
+            return negative()
+    return c
+}
+
+/**Performs the given [action] on each element.
+ *
+ * The operation is _terminal_.*/
+inline fun <T> java.util.Enumeration<T>.forEach(action: (T) -> Unit) {
+    while (this.hasMoreElements())
+        action(this.nextElement())
+}
--- src/main/kotlin/com/losi/create/utility/CGeneral.kt
+++ src/main/kotlin/com/losi/create/utility/CGeneral.kt
@@ -12,73 +12,30 @@ import kotlin.contracts.contract
 inline fun NodeList.forEach(action: (node: Node) -> Unit) {
     for (i in 0 until this.length) action(this.item(i))
 }
+
 /**A quick way to retrieve an attribute value from a [Node]
  * @throws org.w3c.dom.DOMException DOMSTRING_SIZE_ERR: Raised when it would return more characters than fit in a DOMString variable on the implementation platform.*/
 fun Node.getAttribute(name: String): String? = this.attributes.getNamedItem(name).nodeValue
+
 /**Returns a first item of a [NodeList]
  * @throws NullPointerException If the list is empty*/
 fun NodeList.first(): Node = this.item(0)
+
 /**Returns a last item of a [NodeList]
  * @throws NullPointerException If the list is empty*/
 fun NodeList.last(): Node = this.item(this.length - 1)
+
 /**If [this] value is `null` will return the [default]*/
 fun <T> T?.orElse(default: T): T = this ?: default
+
 /**If [this] value is `null` will return the lazy defined [default]*/
 inline fun <T> T?.orElse(default: () -> T): T = this ?: default()
+
 /**If [this] value is `null` will return the [default]*/
 fun Int?.orElse(default: Int) : Int {
     if(this == null)
         return default
     return this
-}
-/**Goes through the list ensuring that all elements in it are equal to each other
- * @return First value of the list, if not all values are equal returns [negative]*/
-inline fun <T: Comparable<T>> List<T>.assertAllEqual(negative: () -> T): T {
-    if(this.isEmpty())
-        return negative()
-    val c: T = this[0]
-    this.forEach { if(it.compareTo(c) != 0) return it }
-    return negative()
-}
-/**Goes through the array ensuring that all elements in it are equal to each other
- * @return First value of the array, if not all values are equal returns [negative]*/
-inline fun <T: Comparable<T>> Array<T>.assertAllEqual(negative: () -> T): T {
-    if(this.isEmpty())
-        return negative()
-    val c: T = this[0]
-    this.forEach { if(it.compareTo(c) != 0) return it }
-    return negative()
-}
-/**Goes through the sequence ensuring that all elements in it are equal to each other
- * @return First value of the sequence, if not all values are equal returns [negative]*/
-inline fun <T: Comparable<T>> Sequence<T>.assertAllEqual(negative: () -> T): T {
-    val it = this.iterator()
-    if(!it.hasNext())
-        return negative()
-    val c: T = it.next()
-    while (it.hasNext())
-        if(c.compareTo(it.next()) != 0)
-            return negative()
-    return c
-}
-/**A variation of [chunked] but reusing the same list to avoid extra allocation*/
-fun <T> Sequence<T>.chunkedReuse(size: Int) = sequence {
-    require(size > 0) { "Chunk size must be positive" }
-    val buffer = mutableListOf<T>()
-    var i = 0
-    for (item in this@chunkedReuse) {
-        if (i == size) { yield(buffer); i = 0 }
-
-        if(buffer.size == i)
-            buffer.add(item)
-        else buffer[i] = item
-        ++i
-    }
-    if (i > 0) {
-        for(j in i until size)
-            buffer.removeLast()
-        yield(buffer)
-    }
 }
 /**A variation of [startsWith] but starting [fromIndex] instead of the start of the string*/
 fun String.startsWithFrom(subString: String, fromIndex: Int): Boolean {
@@ -90,19 +47,12 @@ fun String.startsWithFrom(subString: String, fromIndex: Int): Boolean {
     }
     return true
 }
+
 /**Creates an 64-bit hash intend of the standard 32-bit
  *
  * It is created from hashes of values in the [Pair]*/
 fun <T, R> Pair<T, R>.longHashCode() =
     ((this.first.hashCode().toLong() and 0xFFFFFFFFL) or (this.second.hashCode().toLong() shl 32))
-/**Finds and returns the first element in the [Map] that meats the [condition], if nothing is found will return `null`*/
-inline fun <K, F> Map<K, F>.findFirst(condition: (Map.Entry<K, F>) -> Boolean): Map.Entry<K, F>? {
-    this.forEach {
-        if(condition(it))
-            return it
-    }
-    return null
-}
 /**An overload of [run] the is combined with an assertion that the value it's used on is not null*/
 inline fun <T> T?.mustRun(action: T.() -> Unit) {
     assert(this != null) { "The element was supposed to be set" }
@@ -131,12 +81,4 @@ fun <K, V> Sequence<Pair<K, V>>.toMutableMap(): MutableMap<K, V> {
     val map = mutableMapOf<K, V>()
     this.forEach { (key, value) -> map[key] = value }
     return map
-}
-
-/**Performs the given [action] on each element.
- *
- * The operation is _terminal_.*/
-inline fun <T> java.util.Enumeration<T>.forEach(action: (T) -> Unit) {
-    while (this.hasMoreElements())
-        action(this.nextElement())
 }
