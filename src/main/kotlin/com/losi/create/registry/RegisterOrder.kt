cdfc7b19fb47a093a4079d36848667a88709b466
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2026-06-12 22:15:07 +0200

    Stop the error

--- src/main/kotlin/com/losi/create/registry/RegisterOrder.kt
+++ src/main/kotlin/com/losi/create/registry/RegisterOrder.kt
@@ -1,3 +1,4 @@
+@file:Suppress("unused")
 package com.losi.create.registry
 
 import com.losi.create.utility.exclude
@@ -5,17 +6,63 @@ import com.losi.create.utility.orElse
 import com.losi.create.utility.toMutableMap
 import java.util.UUID
 import java.util.function.Consumer
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+import kotlin.uuid.toKotlinUuid
 
 /**Used to construct pipeline of resource processing*/
 object RegisterOrder {
-    /**All processes that take part in overall resource creation*/
-    private val processes = mutableMapOf<UUID, Pair<Runnable, String>>()
-    /**Marks which processes are dependent on others being finished already*/
-    private val precessRequirements = mutableMapOf<UUID, List<UUID>>()
-    /**Marks which processes are to be done just after others and not when their turn comes, exclusive with [Last][lastPrecesses]*/
-    private val precessesJustAfter = mutableMapOf<UUID, MutableList<UUID>>()
-    /**Marks which processes are to be done last, exclusive with [Just After][precessesJustAfter]*/
-    private val lastPrecesses = mutableListOf<UUID>()
+    /**All processes that take part in overall resource creation*/ @OptIn(ExperimentalUuidApi::class)
+    private val processes = mutableMapOf<Uuid, Pair<Runnable, String>>()
+    /**Marks which processes are dependent on others being finished already*/@OptIn(ExperimentalUuidApi::class)
+    private val precessRequirements = mutableMapOf<Uuid, List<Uuid>>()
+    /**Marks which processes are to be done just after others and not when their turn comes, exclusive with [Last][lastPrecesses]*/@OptIn(ExperimentalUuidApi::class)
+    private val precessesJustAfter = mutableMapOf<Uuid, MutableList<Uuid>>()
+    /**Marks which processes are to be done last, exclusive with [Just After][precessesJustAfter]*/@OptIn(ExperimentalUuidApi::class)
+    private val lastPrecesses = mutableListOf<Uuid>()
+
+    /**Registers a new process to be done during resources loading and it's requirements if there are any
+     * @param process The process to be executed*/
+    fun registerProcess(process: RegisterProcess) {
+        @OptIn(ExperimentalUuidApi::class)
+        registerProcess(process.uuid, process.action, process.name, null)
+    }
+
+    /**Registers a new process to be done during resources loading and it's requirements if there are any
+     * @param process The process to be executed
+     * @param requirements The additional requirements of this process*/
+    fun registerProcess(process: RegisterProcess, requirements: Requirements.() -> Unit) {
+        @OptIn(ExperimentalUuidApi::class)
+        registerProcess(process.uuid, process.action, process.name, Consumer<Requirements> { it.requirements() })
+    }
+
+    /**Registers a new process to be done during resources loading and it's requirements if there are any
+     * @param process The process to be executed
+     * @param requirements The additional requirements of this process*/
+    fun registerProcess(process: RegisterProcess, requirements: Consumer<Requirements>?) {
+        @OptIn(ExperimentalUuidApi::class)
+        registerProcess(process.uuid, process.action, process.name, requirements)
+    }
+
+    /**Registers a new process to be done during resources loading and it's requirements if there are any
+     * @param uuid Used to identify a specific process
+     * @param action The process in question to be registered
+     * @param name A name for the process for easier human identification
+     * @param requirements The additional requirements of this process*/
+    @OptIn(ExperimentalUuidApi::class)
+    fun registerProcess(uuid: Uuid, action: Runnable, name: String = uuid.toString(), requirements: Requirements.() -> Unit) {
+        registerProcess(uuid, action, name, Consumer<Requirements> { it.requirements() })
+    }
+
+    /**Registers a new process to be done during resources loading and it's requirements if there are any
+     * @param uuid Used to identify a specific process
+     * @param action The process in question to be registered
+     * @param name A name for the process for easier human identification
+     * @param requirements The additional requirements of this process*/
+    fun registerProcess(uuid: UUID, action: Runnable, name: String = uuid.toString(), requirements: Requirements.() -> Unit) {
+        @OptIn(ExperimentalUuidApi::class)
+        registerProcess(uuid.toKotlinUuid(), action, name, Consumer<Requirements> { it.requirements() })
+    }
 
     /**Registers a new process to be done during resources loading and it's requirements if there are any
      * @param uuid Used to identify a specific process
@@ -23,17 +70,28 @@ object RegisterOrder {
      * @param name A name for the process for easier human identification
      * @param requirements The additional requirements of this process*/
     fun registerProcess(uuid: UUID, action: Runnable, name: String = uuid.toString(), requirements: Consumer<Requirements>? = null) {
+        @OptIn(ExperimentalUuidApi::class)
+        registerProcess(uuid.toKotlinUuid(), action, name, requirements)
+    }
+
+    /**Registers a new process to be done during resources loading and it's requirements if there are any
+     * @param uuid Used to identify a specific process
+     * @param action The process in question to be registered
+     * @param name A name for the process for easier human identification
+     * @param requirements The additional requirements of this process*/
+    @OptIn(ExperimentalUuidApi::class)
+    fun registerProcess(uuid: Uuid, action: Runnable, name: String = uuid.toString(), requirements: Consumer<Requirements>? = null) {
         require(processes[uuid] == null) { "Process $uuid is already registered" }
 
         processes[uuid] = Pair(action, name)
 
-        val dependency = mutableListOf<UUID>()
-        var rightAfter: UUID? = null
+        val dependency = mutableListOf<Uuid>()
+        var rightAfter: Uuid? = null
         var doLast = false
 
         requirements?.accept(object : Requirements {
-            override fun dependsOn(uuid: UUID) { dependency.add(uuid) }
-            override fun justAfter(@Suppress("PARAMETER_NAME_CHANGED_ON_OVERRIDE") myUuid: UUID) {
+            override fun dependsOn(uuid: Uuid) { dependency.add(uuid) }
+            override fun justAfter(@Suppress("PARAMETER_NAME_CHANGED_ON_OVERRIDE") myUuid: Uuid) {
                 check(!doLast) { "This process was already marked to be done among the last" }
                 require(!(precessesJustAfter[myUuid]?.contains(uuid)?: false)) { "This element is already requested" }
                 rightAfter = myUuid
@@ -52,12 +110,12 @@ object RegisterOrder {
         if(doLast) lastPrecesses.add(uuid)
     }
 
-    internal fun precesses(): Sequence<Pair<Runnable, String>> {
+    internal fun precesses(): Sequence<Pair<Runnable, String>> = @OptIn(ExperimentalUuidApi::class) run {
         val done = processes.keys.asSequence().map { it to false }.toMutableMap()
         val doneAfter = precessesJustAfter.values.flatten()
 
         return sequence {
-            fun trackBack(uuid: UUID): Sequence<UUID> = sequence {
+            fun trackBack(uuid: Uuid): Sequence<Uuid> = sequence {
                 precessRequirements[uuid]?.let { list ->
                     list.forEach {
                         if(done[it] == false)
@@ -98,8 +156,27 @@ object RegisterOrder {
 
     /**Meant for [processes] ut is used to specify requirements of a process*/
     interface Requirements {
-        fun dependsOn(uuid: UUID)
-        fun justAfter(uuid: UUID)
+        fun dependsOn(process: RegisterProcess) {
+            @OptIn(ExperimentalUuidApi::class)
+            dependsOn(process.uuid)
+        }
+        fun dependsOn(uuid: UUID) {
+            @OptIn(ExperimentalUuidApi::class)
+            dependsOn(uuid.toKotlinUuid())
+        }
+        @OptIn(ExperimentalUuidApi::class)
+        fun dependsOn(uuid: Uuid)
+        fun justAfter(process: RegisterProcess) {
+            @OptIn(ExperimentalUuidApi::class)
+            justAfter(process.uuid)
+        }
+        fun justAfter(uuid: UUID) {
+            @OptIn(ExperimentalUuidApi::class)
+            justAfter(uuid.toKotlinUuid())
+        }
+        @OptIn(ExperimentalUuidApi::class)
+        fun justAfter(uuid: Uuid)
+
         fun doLast()
     }
 }
---
+++ src/main/kotlin/com/losi/create/registry/RegisterProcess.kt
@@ -0,0 +1,68 @@
+package com.losi.create.registry
+
+import java.util.UUID
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+import kotlin.uuid.toJavaUuid
+import kotlin.uuid.toKotlinUuid
+
+class RegisterProcess {
+
+    @OptIn(ExperimentalUuidApi::class)
+    val uuid: Uuid
+    val name: String
+    internal val action: Runnable
+
+    @Suppress("unused", "PropertyName")@get:JvmName("getUUID")
+    val UUID: UUID get()
+    {
+        @OptIn(ExperimentalUuidApi::class)
+        return uuid.toJavaUuid()
+    }
+
+    companion object {
+        @OptIn(ExperimentalUuidApi::class)
+        private fun UUID.toUuid() = this.toKotlinUuid()
+    }
+
+    @OptIn(ExperimentalUuidApi::class)@Suppress("unused")
+    constructor(uuid: Uuid, name: String = uuid.toString(), action: Runnable)
+    {
+        this.uuid = uuid
+        this.name = name
+        this.action = action
+    }
+
+    @Suppress("unused")
+    constructor(uuid: UUID, name: String = uuid.toString(), action: Runnable)
+    {
+        @OptIn(ExperimentalUuidApi::class)
+        this.uuid = uuid.toUuid()
+        this.name = name
+        this.action = action
+    }
+
+    constructor(name: String, action: Runnable)
+    {
+        @OptIn(ExperimentalUuidApi::class)
+        this.uuid = Uuid.generateV7()
+        this.name = name
+        this.action = action
+    }
+
+    override infix operator fun equals(other: Any?): Boolean {
+        if(other !is RegisterProcess)
+            return false
+
+        @OptIn(ExperimentalUuidApi::class)
+        return this === other
+    }
+
+    override fun hashCode(): Int {
+        @OptIn(ExperimentalUuidApi::class)
+        var result = uuid.hashCode()
+        result = 31 * result + name.hashCode()
+        result = 31 * result + action.hashCode()
+        return result
+    }
+}
