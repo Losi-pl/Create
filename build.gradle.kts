7d3734e09a28291d75345bc06bfaaef9a2fbe74e
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2026-06-14 01:35:01 +0200

    Newest `coroutines`

--- build.gradle.kts
+++ build.gradle.kts
@@ -100,8 +100,8 @@ dependencies {
     // =================== Kotlin ===================
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.10.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.11.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.11.0")
 
     // =================== Guava ===================
     implementation("com.google.guava:guava:33.6.0-jre")
