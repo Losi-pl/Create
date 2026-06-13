16403259386df2b7e4b3e6612a3c95b6a1cda0f8
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2026-06-13 20:15:53 +0200

    Move the specialized logic into a custom Scene

--- src/main/kotlin/com/losi/create/App.kt
+++ src/main/kotlin/com/losi/create/App.kt
@@ -3,15 +3,10 @@ package com.losi.create
 import com.losi.create.assets.*
 import com.losi.create.graphics.*
 import com.losi.create.registry.LoadingScene
-import com.losi.create.registry.RegisterOrder
 import com.losi.create.utility.*
 import org.lwjgl.glfw.GLFW
-import kotlin.concurrent.thread
 
 private object App {
-    /**The context allowing for the OpenGL operations during the construction of the resources */
-    private var context: GLContext? = null
-
     /**The starting methods of the game starting all other processes
      * @param args The parameters of the game startup
      */
@@ -38,39 +33,10 @@ private object App {
             registerLogic(OnMainThread::callAction)
 
             scene = LoadingScene()
-
-            //Create Assets loading thread
-            context = GLContext(this)
-            thread(name = "Resource Constructing") {
-                context?.use { context ->
-                    context.threadBind()
-                    RegisterOrder.registerAssetPrecesses()
-                    RegisterOrder.precesses().forEach {
-                        it.first.run()
-                    }
-                }
-            }
         }
         window.run()
 
         GLFW.glfwTerminate()
         GLFW.glfwSetErrorCallback(null)?.free()
-    }
-
-    /**The manifests of all processes during loading that are part of this project*/
-    fun RegisterOrder.registerAssetPrecesses() {
-        /*For loading of sources for all file resources*/
-        registerProcess(AssetManager.findingAssetSources)
-
-        /*For registering parsers of file assets*/
-        registerProcess(AssetManager.assetParsers) {
-            dependsOn(AssetManager.findingAssetSources)
-        }
-
-        /*Parsing of the file assets*/
-        registerProcess(AssetManager.assetParsing) {
-            dependsOn(AssetManager.assetParsers)
-        }
-
     }
 }
