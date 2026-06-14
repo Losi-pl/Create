1b9dc6b9c75c347861cefc6cc07a1707d3cdb482
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2026-06-14 15:19:46 +0200

    "What this button do?"

--- src/main/kotlin/com/losi/create/graphics/Scene.kt
+++ src/main/kotlin/com/losi/create/graphics/Scene.kt
@@ -9,22 +9,25 @@ import java.awt.Color
 import java.io.InputStream
 
 abstract class Scene {
+    /**The window this [Scene] is currently connected to*/
     private var _win: Window? = null
+    /**Color of the default background after the render buffer is cleared*/
     private var background: Color = Color.black
-    private var backgroundChanged = false
-
+    /**A flag specifying if the background color has been changed since last render update*/
+    private var backgroundChanged = true
+    /**Title of the Window, inherited from previous [Scene]*/
     protected var title: String
         set(value) { _win?.let { it.title = value } }
         get() = _win?.title?: ""
-
+    /**Default background color*/
     protected var backgroundColor: Color
         set(value) { background = value; backgroundChanged = true }
         get() = background
-
+    /**Targeted refresh rate for this window, inherited from previous [Scene]*/
     protected var targetFPS: UInt
         set(value) { _win?.let { it.targetFPS = value } }
         get() = _win?.targetFPS?: 0u
-
+    /**Size of the window, inherited from previous [Scene]*/
     protected var windowSize: Vector2i
         set(value) { _win?.let { it.size = value } }
         get() = _win?.size?: Vector2i()
@@ -35,21 +38,49 @@ abstract class Scene {
         set(value) { _win?.let { it.icon = value } }
         get() = unaccessible()
 
+    /**For Keyboard events. The specific key used by the user*/
     typealias Key = KeyboardKey
+    /**For Keyboard events. The codes for specific keys*/
     typealias KeyCode = KeyboardKeyCode
+    /**For Keyboard events. The special states that might influence the type of reaction.
+     *
+     * `Shift`, `Control`, `Alt`, etc.*/
     typealias KeyMods = com.losi.create.graphics.gl.KeyMods
+    /**For Keyboard events. Specific action taken by the user.
+     *
+     * `Press`ed, `Releas`ed, `Repeat`ed*/
     typealias KeyAction = KeyboardAction
 
+    /**Called when the scene is first bound to the window used primarily to change the window specific settings or load Scene content if not done before*/
     open fun onSceneInit() { }
+    /**Called when any keyboard related action is taken
+     *
+     * `Key Pressed`, `Key Released`, `Key Double Pressed`
+     * @param key The specific key that this action was called for
+     * @param action The type of action this method was called for
+     * @param mods The extra modifications that can influence the result for this button. See [KeyMods]*/
     open fun onKeyAction(key: Key, action: KeyAction, mods: KeyMods) { }
+    /**Called when any key is pressed down
+     * @param key The specific key that this action was called for
+     * @param mods The extra modifications that can influence the result for this button. See [KeyMods]*/
     open fun onKeyDown(key: Key, mods: KeyMods) { }
+    /**Called when any key is released
+     * @param key The specific key that this action was called for
+     * @param mods The extra modifications that can influence the result for this button. See [KeyMods]*/
     open fun onKeyUp(key: Key, mods: KeyMods) { }
+    /**Called when any key pressed down quickly again
+     * @param key The specific key that this action was called for
+     * @param mods The extra modifications that can influence the result for this button. See [KeyMods]*/
     open fun onKeyRepeat(key: Key, mods: KeyMods) { }
 
+    /**Meant for per frame logic processing, any rendering done during this call will be lost
+     * @param timeDelta Time since last call*/
     open fun logicUpdate(timeDelta: Float) { }
+    /**Meant for frame rendering related logic
+     * @param timeDelta Time since last call*/
     open fun renderUpdate(timeDelta: Float) { }
 
-    /**Creates a thread that runs the specified block of code. This is a variation
+    /**Creates a thread that runs the specified block of code. This is a variation for [Scene]'s automatically connecting with the OpenGL
      * @param start if `true`, the thread is immediately started.
      * @param isDaemon if `true`, the thread is created as a daemon thread. The Java Virtual Machine exits when the only threads running are all daemon threads.
      * @param contextClassLoader - the class loader to use for loading classes and resources in this thread.
@@ -84,6 +115,8 @@ abstract class Scene {
         return thread
     }
 
+    /**The set of per frame logic put in correct order
+     * @param timeDelta Time since last call*/
     internal fun update(timeDelta: Float) {
         val me = this
 
@@ -98,11 +131,13 @@ abstract class Scene {
         glClear(ClearTarget.Color and ClearTarget.Depth)
         me.renderUpdate(timeDelta)
     }
+    /**Used when this scene is being connected to a [Window]*/
     internal fun bindingScene(window: Window) {
         val me = this
         _win = window
         me.onSceneInit()
     }
+    /**Used when this scene is being disconnected from a [Window]*/
     internal fun unbindingScene() {
         _win = null
     }
