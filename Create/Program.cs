76595ea6b3ae7bc3d6ff94e151655584a6755557
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2026-06-21 01:41:33 +0200

    All I need.   Propabyly...

--- Create/Program.cs
+++ Create/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+
+Console.WriteLine("Hello, World!");
+foreach (var val in  Assembly.GetCallingAssembly().GetManifestResourceNames())
+{
+    Console.WriteLine(val);
+}
