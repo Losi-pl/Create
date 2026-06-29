3685163491f8937d22915be46f147b06ecba2b55
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2026-06-29 13:55:14 +0200

    Class `Camera`

---
+++ Create/Graphics/Camera.cs
@@ -0,0 +1,69 @@
+﻿using System.Numerics;
+
+namespace Create.Graphics;
+
+public class Camera
+{
+    public Vector2 Orientation
+    {
+        get;
+        set
+        {
+            if(field == value)
+                return;
+            field = value;
+            RecalculateView();
+        }
+    }
+    
+    public Vector3 Position
+    {
+        get;
+        set
+        {
+            if(field == value)
+                return;
+            field = value;
+            RecalculateView();
+        }
+    }
+    
+    public float ProjectionAngle
+    {
+        get;
+        set
+        {
+            field = value;
+            RecalculateProjection();
+        }
+    }
+
+    public Silk.NET.Maths.Vector2D<int> ScreenDimensions
+    {
+        get;
+        set
+        {
+            if(field == value)
+                return;
+            field = value;
+            RecalculateProjection();
+        }
+    }
+
+    
+    public Matrix4x4 ProjectionMatrix { get; private set; } = Matrix4x4.Identity;
+    public Matrix4x4 ViewMatrix { get; private set; } = Matrix4x4.Identity;
+
+    private void RecalculateView()
+    {
+        var mat = Matrix4x4.CreateTranslation(-Position.X, -Position.Y, -Position.Z);
+        if (Orientation != new Vector2(0, 0))
+            mat *= Matrix4x4.CreateRotationY(Orientation.X * MathF.PI / 180f) * Matrix4x4.CreateRotationX(Orientation.Y * MathF.PI / 180f);
+        ViewMatrix = mat;
+    }
+    private void RecalculateProjection()
+    {
+        ProjectionMatrix = Matrix4x4.CreatePerspectiveFieldOfViewLeftHanded(ProjectionAngle * MathF.PI / 180f, 
+            ScreenDimensions.X / (float)ScreenDimensions.Y, .1f, 512f * 3 / 2);
+    }
+}
--- Create/World/GameSession.cs
+++ Create/World/GameSession.cs
@@ -7,12 +7,19 @@ namespace Create.World;
 
 public sealed class GameSession: Scene
 {
+    private readonly Camera _camera = new();
     private RealmWorld _world = null!;
     private static Mesh _worldMesh = null!;
     
     protected override void OnConnect()
     {
         Title = "Create";
+
+        _camera.ProjectionAngle = 70;
+        _camera.ScreenDimensions = Size;
+
+        _camera.Position = new(0, 3, -3);
+        _camera.Orientation = new(0, -45);
         
         _world = new()
         {
@@ -26,27 +33,17 @@ public sealed class GameSession: Scene
 
         _worldMesh = new ChunkModeler().GenerateModel(_world).ThreadBind();
 
-        var fovRadians = 70f * MathF.PI / 180f;
-        var aspect = Size.X / (float)Size.Y;
-
-        var projection = Matrix4x4.CreatePerspectiveFieldOfViewLeftHanded(fovRadians, aspect, .1f, 512f * 3 / 2);
-        var view = Matrix4x4.CreateLookAtLeftHanded(new(0, 5, -5), new(0, 0, 0), new(0, 1, 0));
-
-        _worldMesh.Shader.SetUniform("projection", projection);
-        _worldMesh.Shader.SetUniform("view", view);
-        _worldMesh.Shader.SetUniform("model", Matrix4X4<float>.Identity);
+        _worldMesh.Shader.SetUniform("projection", _camera.ProjectionMatrix);
+        _worldMesh.Shader.SetUniform("view", _camera.ViewMatrix);
+        _worldMesh.Shader.SetUniform("model", Matrix4x4.CreateTranslation(-.5f, 0, -.5f));
         
         Window.GL.Enable(EnableCap.DepthTest);
     }
     
     public override void WindowResize(Vector2D<int> newSize)
     {
-        var fovRadians = 70f * MathF.PI / 180f;
-        var aspect = Size.X / (float)Size.Y;
-
-        var projection = Matrix4x4.CreatePerspectiveFieldOfViewLeftHanded(fovRadians, aspect, .1f, 512f * 3 / 2);
-        
-        _worldMesh.Shader.SetUniform("projection", projection);
+        _camera.ScreenDimensions = newSize;
+        _worldMesh.Shader.SetUniform("projection", _camera.ProjectionMatrix);
     }
     
     public override void RenderUpdate(double delta)
