c9445c7edef22d92c8347a115f1e6b8d3a43d958
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2026-06-30 21:47:15 +0200

    Extra

--- Create/Math/VectorMath.cs
+++ Create/Math/VectorMath.cs
@@ -8,6 +8,7 @@ public static class VectorMath
     extension(Vector2)
     {
         public static Vector2 operator +(Vector2 v1, Vector2D<int> v2) => new(v1.X + v2.X, v1.Y + v2.Y);
+        public static Vector2 operator +(Vector2 v1, Vector2D<float> v2) => new(v1.X + v2.X, v1.Y + v2.Y);
 
         public static Vector2D<int> ToInt(Vector2 v) => new((int)v.X, (int)v.Y);
     }
