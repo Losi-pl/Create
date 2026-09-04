c328c590fa51a66cd50bae4c8b35d44e6731165b
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2026-09-05 00:50:24 +0200

    Manual load of textures

--- Create/Elements/BlockClasses/Bedrock.cs
+++ Create/Elements/BlockClasses/Bedrock.cs
@@ -1,6 +1,15 @@
-﻿namespace Create.Elements.BlockClasses;
+﻿using Create.Assets;
+
+namespace Create.Elements.BlockClasses;
 
 public class Bedrock : Block
 {
-    
+    private BlockTexture _texture;
+
+    protected override void OnElementRegistered()
+    {
+        _texture = AssetManager.Find<BlockTexture>("create:bedrock").AsSet;
+    }
+
+    public override BlockTexture GetTexture(ref readonly GetTextureArgs args) => _texture;
 }
--- Create/Elements/BlockClasses/Stone.cs
+++ Create/Elements/BlockClasses/Stone.cs
@@ -1,6 +1,15 @@
-﻿namespace Create.Elements.BlockClasses;
+﻿using Create.Assets;
+
+namespace Create.Elements.BlockClasses;
 
 public class Stone : Block
 {
-    
+    private BlockTexture _texture;
+
+    protected override void OnElementRegistered()
+    {
+        _texture = AssetManager.Find<BlockTexture>("create:stone").AsSet;
+    }
+
+    public override BlockTexture GetTexture(ref readonly GetTextureArgs args) => _texture;
 }
