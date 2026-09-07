cdcae503ea45832f22511052547444a2c64c651f
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2026-09-07 02:05:42 +0200

    Facet type `ColoredTextureFace`

---
+++ Create/Graphics/Block/ColoredTextureFace.cs
@@ -0,0 +1,69 @@
+﻿using System.Drawing;
+using CommunityToolkit.HighPerformance.Helpers;
+using Create.Assets;
+using Create.World;
+using Silk.NET.Maths;
+
+using ModelData = (System.Collections.Generic.List<Silk.NET.Maths.Vector3D<float>> Positions,
+                   System.Collections.Generic.List<Silk.NET.Maths.Vector2D<float>> UVs,
+                   System.Collections.Generic.List<uint> Textures,
+                   System.Collections.Generic.List<Silk.NET.Maths.Vector3D<float>> Colors,
+                   System.Collections.Generic.List<uint> Triangles);
+
+namespace Create.Graphics.Block;
+
+public class ColoredTextureFace(BlockTexture texture, Color color) : IBlockModelFace
+{
+    public BlockTexture Texture => texture;
+    public Color Color => Color.FromArgb(byte.MaxValue, color);
+    
+    private static Shader Shader
+    {
+        get => field ??= AssetManager.Find<Shader>("create:blocks/colored-texture") is { IsSet: true, AsSet: var set } ? set : throw new Exception("Shader not found");
+    } = null!;
+    
+    static object IBlockModelFace.CreateNewModelData() => new ModelData([], [], [], [], []);
+
+    void IBlockModelFace.AddToModel<T>(object modelData, uint vertexCount, uint triangleCount, WorldModeler.FillOutData<T> fillOutData, Vector3D<long> blockPosition, T fillOutArg)
+    {
+        if(!modelData.TryUnbox<ModelData>(out var model))
+            throw new ArgumentException("ModelData is not a valid type");
+        
+        Span<Vector3D<float>> positions = stackalloc Vector3D<float>[(int)vertexCount];
+        Span<Vector2D<float>> uvs = stackalloc Vector2D<float>[(int)vertexCount];
+        Span<uint> trianglesElem = stackalloc uint[(int)triangleCount * 3];
+        Span<uint> textureInd = stackalloc uint[(int)vertexCount];
+        Span<Vector3D<float>> colors = stackalloc Vector3D<float>[(int)vertexCount];
+
+        fillOutData(positions, uvs, trianglesElem, blockPosition, fillOutArg);
+        {
+            var prevCount = (uint)model.Positions.Count;
+            for (var i = 0; i < trianglesElem.Length; i++)
+                trianglesElem[i] += prevCount;
+            
+            textureInd.Fill(texture.Index);
+            var col = Color.AsFloatVector();
+            colors.Fill(col.RGB);
+        }
+        
+        model.Positions.AddRange(positions);
+        model.UVs.AddRange(uvs);
+        model.Textures.AddRange(textureInd);
+        model.Colors.AddRange(colors);
+        model.Triangles.AddRange(trianglesElem);
+    }
+
+    static Mesh IBlockModelFace.FinishModel(object modelData)
+    {
+        if(!modelData.TryUnbox<ModelData>(out var model))
+            throw new ArgumentException("modelData is not a valid model data type");
+        
+        return Mesh.Create(Shader).ManualFillOut()
+            .SetAttribute("pos", model.Positions.ToArray())
+            .SetAttribute("uvPos", model.UVs.ToArray())
+            .SetAttribute("atlasInd", model.Textures.ToArray())
+            .SetAttribute("color", model.Colors.ToArray())
+            .Triangles(model.Triangles.ToArray())
+            .Finish();
+    }
+}
---
+++ Create/Resources/main/create/shaders/blocks/colored-texture.frag
@@ -0,0 +1,13 @@
+#version 330
+
+in vec2 uv;
+in vec3 col;
+flat in uint ind;
+
+out vec4 fragColor;
+
+uniform sampler2DArray atlas;
+
+void main() {
+    fragColor = texture(atlas, vec3(uv, ind)) * vec4(col, 1.0);
+}
