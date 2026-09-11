b8c0c0efb02fb790ca61f078a5cc3be4ec24e639
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2026-09-11 18:48:46 +0200

    Method `RunGraphics` added to `Task`

---
+++ Create/Graphics/TaskGL.cs
@@ -0,0 +1,51 @@
+﻿namespace Create.Graphics;
+
+public static class TaskGL
+{
+    extension(Task)
+    {
+        public static Task RunGraphics(Action action)
+        {
+            ArgumentNullException.ThrowIfNull(action);
+            var task = new TaskCompletionSource(TaskCreationOptions.RunContinuationsAsynchronously);
+            var context = Window.IsMainThread ? new GraphicContext() : Window.Query(() => new GraphicContext()).Result;
+            var thread = new Thread(() =>
+            {
+                try { context.ThreadBind(); }
+                catch (Exception e) { task.SetException(e); return; }
+                
+                try { action(); task.SetResult(); }
+                catch (Exception e) { task.SetException(e); }
+                finally
+                {
+                    context.Unbind();
+                    context.Dispose();
+                }
+            }) { IsBackground = true };
+            thread.Start();
+            return task.Task;
+        }
+
+        public static Task<T> RunGraphics<T>(Func<T> func)
+        {
+            ArgumentNullException.ThrowIfNull(func);
+            var task = new TaskCompletionSource<T>(TaskCreationOptions.RunContinuationsAsynchronously);
+            var context = Window.IsMainThread ? new GraphicContext() : Window.Query(() => new GraphicContext()).Result;
+            var thread = new Thread(() =>
+            {
+                try { context.ThreadBind(); }
+                catch (Exception e) { task.SetException(e); return; }
+                
+                try { task.SetResult(func()); }
+                catch (Exception e) { task.SetException(e); }
+                finally
+                {
+                    context.Unbind();
+                    context.Dispose();
+                }
+            }) { IsBackground = true };
+            thread.Start();
+            return task.Task;
+        }
+    }
+}
