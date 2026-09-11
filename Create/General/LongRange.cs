f06faca67cfa1ad23f2d22916827189e5ee90700
Losi-pl <83635228+Losi-pl@users.noreply.github.com>
2026-09-11 21:34:18 +0200

    Missing methods in `LongRange`

--- Create/General/LongRange.cs
+++ Create/General/LongRange.cs
@@ -1,8 +1,9 @@
-﻿using Range = Arch.Core.Range;
+﻿using System.Diagnostics.CodeAnalysis;
+using Range = Arch.Core.Range;
 
 namespace Create.General;
 
-public struct LongRange(long start, long end) : IEquatable<LongRange>
+public readonly struct LongRange(long start, long end) : IEquatable<LongRange>
 {
     public long Start => start;
     public long End => end;
@@ -10,4 +11,7 @@ public struct LongRange(long start, long end) : IEquatable<LongRange>
     public bool Equals(LongRange other) => this == other;
     public static bool operator !=(LongRange left, LongRange right) => !(left == right);
     public static bool operator ==(LongRange left, LongRange right) => left.Start == right.Start && left.End == right.End;
+
+    public override int GetHashCode() => HashCode.Combine(Start, End);
+    public override bool Equals([NotNullWhen(true)] object? obj) => obj is LongRange other && this == other;
 }
